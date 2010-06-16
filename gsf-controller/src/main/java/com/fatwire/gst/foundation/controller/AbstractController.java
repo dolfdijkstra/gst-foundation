f3fcb5a8402353faa76a84ac034cdb546aed6856
Dolf Dijkstra <Dolf.Dijkstra@xs4all.nl>
2010-06-16 21:09:04 +0000

    First complete version (untested)

--- gsf-controller/src/main/java/com/fatwire/gst/foundation/controller/AbstractController.java
+++ gsf-controller/src/main/java/com/fatwire/gst/foundation/controller/AbstractController.java
@@ -1,46 +1,144 @@
 package com.fatwire.gst.foundation.controller;
 
-import COM.FutureTense.Interfaces.*;
-import COM.FutureTense.XML.Template.Seed;
-import com.fatwire.developernet.CSRuntimeException;
+import static COM.FutureTense.Interfaces.Utilities.goodString;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import COM.FutureTense.Interfaces.FTValList;
+import COM.FutureTense.Interfaces.ICS;
+import COM.FutureTense.Interfaces.IPS;
+import COM.FutureTense.XML.Template.Seed2;
 
-public abstract class AbstractController implements Seed
-{
-    protected static final Log LOG = LogFactory.getLog("com.fatwire.gst.foundation.controller");
+import com.fatwire.gst.foundation.CSRuntimeException;
+import com.openmarket.xcelerate.publish.Render;
 
-    public final String Execute(FTValList vIn, FTValList vOut)
-    {
-        FTVAL obj = vIn.getVal("_ICS");
-        final ICS ics = (ICS)obj.GetObj();
+/**
+ * 
+ * @author Tony Field
+ * @author Dolf.Dijkstra
+ * @since Jun 16, 2010
+ */
 
-        try
-        {
-            execute(ics);
-        }
-        catch(CSRuntimeException e)
-        {
-            handleCSRuntimeException(ics, e);
+public abstract class AbstractController implements Seed2 {
+    protected static final Log LOG = LogFactory
+            .getLog("com.fatwire.gst.foundation.controller");
+
+    public static String STATUS_HEADER = "X-Fatwire-Status";
+
+    protected ICS ics;
+
+    /*
+     * (non-Javadoc)
+     * @see COM.FutureTense.XML.Template.Seed2#SetAppLogic(COM.FutureTense.Interfaces.IPS)
+     */
+    public void SetAppLogic(final IPS ips) {
+        ics = ips.GetICSObject();
+
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see COM.FutureTense.XML.Template.Seed#Execute(COM.FutureTense.Interfaces.FTValList, COM.FutureTense.Interfaces.FTValList)
+     */
+    public final String Execute(final FTValList vIn, final FTValList vOut) {
+        try {
+            doExecute();
+        } catch (final Exception e) {
+            handleException(e);
         }
 
         return "";
     }
 
     /**
-     * Executes the core business logic of the controller.
-     *
-     * @param ics
-     * @throws CSRuntimeException may throw a CSRuntimeException which is handled by handleCSRuntimeException
+     * Sends the http status code to the user-agent
+     * 
+     * @param code the http response code
+     * @return String to stream
      */
-    abstract protected void execute(ICS ics);
+    protected String sendError(final int code, final Exception e) {
+        LOG.debug(code + " status code sent due to exception " + e.toString(),
+                e);
+        switch (code) { //all the http status codes, we may restrict the list to error and redirect
+        case 100:
+        case 101:
+        case 200:
+        case 201:
+        case 202:
+        case 203:
+        case 204:
+        case 205:
+        case 206:
+        case 300:
+        case 301:
+        case 302:
+        case 303:
+        case 304:
+        case 306:
+        case 307:
+        case 400:
+        case 401:
+        case 402:
+        case 403:
+        case 404:
+        case 405:
+        case 406:
+        case 407:
+        case 408:
+        case 409:
+        case 410:
+        case 411:
+        case 412:
+        case 413:
+        case 414:
+        case 415:
+        case 416:
+        case 417:
+        case 450:
+        case 500:
+        case 501:
+        case 502:
+        case 503:
+        case 504:
+        case 505:
+            ics.StreamHeader(STATUS_HEADER, Integer.toString(code));
+            break;
+        default:
+            ics.StreamHeader(STATUS_HEADER, "500");
+            break;
+        }
+        Render.UnknownDeps(ics);//failure case might be corrected on next publish or save
+        String element = null;
+
+        if (goodString(ics.GetVar("site"))
+                && ics.IsElement(ics.GetVar("site") + "/ErrorHandler/" + code)) {
+            element = ics.GetVar("site") + "/ErrorHandler/" + code;
+        } else if (ics.IsElement("GST/ErrorHandler/" + code)) {
+            element = "GST/ErrorHandler/" + code;
+        }
+        if (element != null) {
+            ics.SetObj("com.fatwire.gst.foundation.exception", e);
+            ics.CallElement(element, null);
+        }
+        ics.SetErrno(-500); // or whatever make sense
+
+        return null;
+
+    }
 
     /**
-     * Handles teh exception, doing what is required
+     * Executes the core business logic of the controller.
      *
-     * @param ics ics context
+
+     * @throws CSRuntimeException may throw a CSRuntimeException which is handled by handleCSRuntimeException
+     */
+    abstract protected void doExecute();
+
+    /**
+     * Handles the exception, doing what is required
+     *
      * @param e exception
      */
-    abstract protected void handleCSRuntimeException(ICS ics, CSRuntimeException e);
+    abstract protected void handleException(Exception e);
 }
--- gsf-controller/src/main/java/com/fatwire/gst/foundation/controller/BaseController.java
+++ gsf-controller/src/main/java/com/fatwire/gst/foundation/controller/BaseController.java
@@ -8,30 +8,59 @@
  */
 package com.fatwire.gst.foundation.controller;
 
-import COM.FutureTense.Interfaces.ICS;
+import static COM.FutureTense.Interfaces.Utilities.goodString;
+import static com.fatwire.cs.core.db.Util.parseJdbcDate;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Enumeration;
+import java.util.List;
+
 import COM.FutureTense.Util.ftErrors;
+import COM.FutureTense.Util.ftMessage;
+
 import com.fatwire.assetapi.data.AssetId;
-import com.fatwire.developernet.CSRuntimeException;
-import com.fatwire.developernet.facade.runtag.example.render.CallTemplate;
+import com.fatwire.cs.core.db.PreparedStmt;
+import com.fatwire.cs.core.db.StatementParam;
+import com.fatwire.gst.foundation.CSRuntimeException;
+import com.fatwire.gst.foundation.facade.runtag.render.CallTemplate;
+import com.fatwire.gst.foundation.facade.sql.Row;
+import com.fatwire.gst.foundation.facade.sql.SqlHelper;
 import com.openmarket.xcelerate.asset.AssetIdImpl;
 
-import java.util.*;
-
-import static COM.FutureTense.Interfaces.Utilities.goodString;
-
 /**
- * <CALLJAVA CLASS="com.fatwire.gst.foundation.controller.BaseController" />
+ * 
+ * <p>This is the controller (dispatcher) that dispatches the request to the correct asset/template combination based on the path field for a 
+ * Web Referenceable Asset.</p>
+ *  <p>This controller should be called from an outer XML element via the <tt>CALLJAVA</tt> tag: 
+ *   * &lt;CALLJAVA CLASS="com.fatwire.gst.foundation.controller.BaseController" /&gt;
  *
  * @author Tony Field
+ * @author Dolf Dijkstra
  * @since Jun 10, 2010
  */
-public class BaseController extends AbstractController
-{
-    protected void execute(ICS ics)
-    {
-        final AssetId id = resolveAssetId(ics);
-        String templatename = lookupTemplateForAsset(ics, id);
-        callTemplate(ics, id, templatename);
+public class BaseController extends AbstractController {
+    @Override
+    protected void doExecute() {
+        final AssetId id = resolveAssetId();
+        if (id == null) {
+            throw new CSRuntimeException("No asset found", 404);
+        }
+        final String templatename = lookupTemplateForAsset(id);
+        if (templatename == null) {
+            throw new CSRuntimeException("No template found", 404);
+        }
+        callTemplate(id, templatename);
+    }
+
+    @Override
+    protected void handleException(final Exception e) {
+        if (e instanceof CSRuntimeException) {
+            handleCSRuntimeException((CSRuntimeException) e);
+        } else {
+            sendError(500, e);
+        }
     }
 
     /**
@@ -45,65 +74,124 @@ public class BaseController extends AbstractController
      * @param ics ics context
      * @param e exception
      */
-    protected void handleCSRuntimeException(ICS ics, CSRuntimeException e)
-    {
-        switch(e.getErrno())
-        {
-            case ftErrors.badparams:
-                ics.StreamHeader("X-Fatwire-Status", "400");
-                LOG.debug("400 status code sent due to exception " + e.toString(), e);
-                break;
-            case ftErrors.pagenotfound:
-                ics.StreamHeader("X-Fatwire-Status", "404");
-                LOG.debug("404 status code sent due to exception " + e.toString(), e);
-                break;
-            case ftErrors.noprivs:
-                ics.StreamHeader("X-Fatwire-Status", "403");
-                LOG.debug("404 status code sent due to exception " + e.toString(), e);
-                break;
-            default:
-                LOG.error("Unhandled CSRuntimeException caught by BaseController.  Re-throwing", e);
+    protected void handleCSRuntimeException(final CSRuntimeException e) {
+        switch (e.getErrno()) {
+        case ftErrors.badparams:
+            sendError(400, e);
+            break;
+        case ftErrors.pagenotfound:
+            sendError(404, e);
+            break;
+        case ftErrors.noprivs:
+            sendError(403, e);
+            break;
+        default:
+            sendError(500, e);
+            break;
         }
     }
 
-
-    protected AssetId resolveAssetId(ICS ics)
-    {
+    protected AssetId resolveAssetId() {
         final AssetId id;
-        if(goodString(ics.GetVar("virtual-webroot")) && goodString(ics.GetVar("url-path")))
-        {
-            id = resolveAssetFromUrl(ics.GetVar("virtual-webroot"), ics.GetVar("url-path"));
-        }
-        else if(goodString(ics.GetVar("c")) && goodString(ics.GetVar("cid"))) // handle these to be nice
-        {
-            id = new AssetIdImpl(ics.GetVar("c"), Long.valueOf(ics.GetVar("cid")));
-        }
-        else
-        {
-            throw new CSRuntimeException("Missing required param virtual-webroot & url-path or c & cid.", ftErrors.badparams);
+        if (goodString(ics.GetVar("virtual-webroot"))
+                && goodString(ics.GetVar("url-path"))) {
+            id = resolveAssetFromUrl(ics.GetVar("virtual-webroot"), ics
+                    .GetVar("url-path"));
+        } else if (goodString(ics.GetVar("c")) && goodString(ics.GetVar("cid"))) { // handle these to be nice
+            id = new AssetIdImpl(ics.GetVar("c"), Long.parseLong(ics
+                    .GetVar("cid")));
+        } else {
+            if (goodString(ics.GetVar("virtual-webroot"))
+                    && goodString(ics.GetVar("url-path"))) {
+                throw new CSRuntimeException(
+                        "Missing required param virtual-webroot & url-path.",
+                        ftErrors.badparams);
+            } else {
+                throw new CSRuntimeException("Missing required param c & cid.",
+                        ftErrors.badparams);
+
+            }
+
         }
         return id;
     }
 
-    protected AssetId resolveAssetFromUrl(String virtual_webroot, String url_path)
-    {
-        return null; // todo: implement
+    static final String REGISTRY_SELECT = "SELECT assettype ,assetid ,startdate ,enddate FROM GSTUrlRegistry WHERE opt_vwebroot=? AND opt_url_path=? ORDER BY startdate,enddate";
+
+    static final String REGISTRY_TABLE = "GSTUrlRegistry";
+
+    protected AssetId resolveAssetFromUrl(final String virtual_webroot,
+            final String url_path) {
+
+        final PreparedStmt stmt = new PreparedStmt(REGISTRY_SELECT, Collections
+                .singletonList(REGISTRY_TABLE));
+        stmt.setElement(0, REGISTRY_TABLE, "opt_vwebroot");
+        stmt.setElement(1, REGISTRY_TABLE, "opt_url_path");
+        final StatementParam param = stmt.newParam();
+        param.setString(0, virtual_webroot);
+        param.setString(1, url_path);
+        final Date now = new Date();
+        for (final Row asset : SqlHelper.select(ics, stmt, param)) {
+
+            final String assettype = asset.getString("assettype");
+            final String assetid = asset.getString("assetid");
+            if (inRange(asset, now)) {
+                return new AssetIdImpl(assettype, Long.parseLong(assetid));
+            }
+        }
+
+        return null;
     }
 
-    protected String lookupTemplateForAsset(ICS ics, AssetId id)
-    {
+    //TODO: Finish this method
+    protected boolean inRange(final Row asset, final Date now) {
+        final Date startdate = asset.getString("startdate") != null ? parseJdbcDate(asset
+                .getString("startdate"))
+                : null;
+        final Date enddate = asset.getString("enddate") != null ? parseJdbcDate(asset
+                .getString("enddate"))
+                : null;
 
-        /*
-        select template from id.getType() where id = id.getId()
-        */
-        return null; // todo: implement
+        if (startdate != null || enddate != null) {
+            if (startdate == null) {
+                if (enddate.before(now)) {
+                    return false;
+                }
+            } else {
+                if (startdate.after(now)) {
+                    return false;
+                }
+            }
+
+        }
+        return true;
     }
 
-    private static final List<String> CALLTEMPLATE_EXCLUDE_VARS = Arrays.asList("c", "cid", "eid", "seid", "packedargs", "variant", "context", "pagename", "childpagename", "site", "tid", "virtual-webroot", "url-path");
+    protected String lookupTemplateForAsset(final AssetId id) {
+        final String select = "SELECT template FROM " + id.getType()
+                + " where id=?";
 
-    protected void callTemplate(ICS ics, AssetId id, String tname)
-    {
-        CallTemplate ct = new CallTemplate();
+        final PreparedStmt stmt = new PreparedStmt(select, Collections
+                .singletonList(id.getType()));
+        stmt.setElement(0, id.getType(), "id");
+
+        final StatementParam param = stmt.newParam();
+        param.setLong(0, id.getId());
+
+        for (final Row row : SqlHelper.select(ics, stmt, param)) {
+            return row.getString("template");
+        }
+        return null;
+    }
+
+    private static final List<String> CALLTEMPLATE_EXCLUDE_VARS = Arrays
+            .asList("c", "cid", "eid", "seid", "packedargs", "variant",
+                    "context", "pagename", "childpagename", "site", "tid",
+                    "virtual-webroot", "url-path");
+
+    @SuppressWarnings("unchecked")
+    protected void callTemplate(final AssetId id, final String tname) {
+        final CallTemplate ct = new CallTemplate();
         ct.setSite(ics.GetVar("site"));
         ct.setSlotname("wrapper");
         ct.setTid(ics.GetVar("eid"));
@@ -111,30 +199,36 @@ public class BaseController extends AbstractController
         ct.setAsset(id);
         ct.setTname(tname);
         ct.setContext("");
-        ct.setStyle("pagelet");
-        String variant = ics.GetVar("variant");
-        if(variant != null && variant.length() > 0)
-        {
+        String target = tname.startsWith("/") ? ics.GetVar("site") + "/"
+                + tname : ics.GetVar("site") + "/" + id.getType() + "/" + tname; //typeless or not
+        
+        if (ics.isCacheable(ics.GetVar(ftMessage.PageName))) {
+            ct.setStyle("element"); // call as element when current is caching    
+        } else if (ics.isCacheable(target)) {
+            ct.setStyle("pagelet"); // call as pagelet when current is not caching and target is
+        } else {
+            ct.setStyle("element"); // call as pagelet when current is not caching and target is not
+        }
+
+        final String variant = ics.GetVar("variant");
+        if (variant != null && variant.length() > 0) {
             ct.setVariant(variant);
         }
-        String packedargs = ics.GetVar("packedargs");
-        if(packedargs != null && packedargs.length() > 0)
-        {
+        final String packedargs = ics.GetVar("packedargs");
+        if (packedargs != null && packedargs.length() > 0) {
             ct.setPackedargs(packedargs);
         }
 
-        Enumeration vars = ics.GetVars();
-        while(vars.hasMoreElements())
-        {
-            String varnane = (String)vars.nextElement();
-            if(!CALLTEMPLATE_EXCLUDE_VARS.contains(varnane))
-            {
+        final Enumeration<String> vars = ics.GetVars();
+        while (vars.hasMoreElements()) {
+            final String varnane = vars.nextElement();
+            if (!CALLTEMPLATE_EXCLUDE_VARS.contains(varnane)) {
+                //TODO: handle pagecriteria if we are calling cacheable
                 ct.setArgument(varnane, ics.GetVar(varnane));
             }
         }
 
         ct.execute(ics);
     }
-
 
 }
