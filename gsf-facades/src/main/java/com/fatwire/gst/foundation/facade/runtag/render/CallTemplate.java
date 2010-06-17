e0058e2d36b83bf6b9b1fe604e81cce81ef5dbde
Dolf Dijkstra <Dolf.Dijkstra@xs4all.nl>
2010-06-17 08:44:10 +0000

    Adding enums and create constructor with reasonable defaults

--- gsf-facades/src/main/java/com/fatwire/gst/foundation/facade/runtag/render/CallTemplate.java
+++ gsf-facades/src/main/java/com/fatwire/gst/foundation/facade/runtag/render/CallTemplate.java
@@ -15,62 +15,99 @@ import org.apache.commons.logging.LogFactory;
 /**
  * CallTemplate tag
  * <p/>
- * <RENDER.CALLTEMPLATE
- * SITE="site name"
- * SLOTNAME="name of slot"
- * TID="caller Template or CSElement id"
- * [TTYPE="caller Template or CSElement"]
- * [C="asset type"]
- * [CID="asset id"]
- * [TNAME="target Template or CSElement name"]
- * [CONTEXT="context override"]
- * [STYLE="pagelet or element"]
- * [VARIANT="template variant name"]
- * [PACKEDARGS="packed arguments"]>
+ * <RENDER.CALLTEMPLATE SITE="site name" SLOTNAME="name of slot"
+ * TID="caller Template or CSElement id" [TTYPE="caller Template or CSElement"]
+ * [C="asset type"] [CID="asset id"] [TNAME="target Template or CSElement name"]
+ * [CONTEXT="context override"] [STYLE="pagelet or element"]
+ * [VARIANT="template variant name"] [PACKEDARGS="packed arguments"]>
  * <p/>
  * [<RENDER.ARGUMENT NAME="variable1" VALUE="value1"/>]
  * <p/>
  * </RENDER.CALLTEMPLATE>
- *
+ * 
  * @author Tony Field
  * @since Jun 10, 2010
  */
-public class CallTemplate extends TagRunnerWithArguments
-{
-    private static Log LOG = LogFactory.getLog(GetTemplateUrl.class);
+public class CallTemplate extends TagRunnerWithArguments {
+	private static Log LOG = LogFactory.getLog(GetTemplateUrl.class);
 
-    public CallTemplate()
-    {
-        super("RENDER.CALLTEMPLATE");
-    }
+	public enum Style {
+		element, pagelet, embedded
+	}
 
-    public void setSite(String s) { set("SITE", s); }
+	public enum Type {
+		Template, CSElement
+	}
 
-    public void setSlotname(String s) { set("SLOTNAME", s); }
+	public CallTemplate() {
+		super("RENDER.CALLTEMPLATE");
+	}
 
-    public void setTid(String s) { set("TID", s); }
+	
+	/**
+	 * Sets up CallTemplate with default <tt>Style.element</tt>
+	 * 
+	 * @param slotname
+	 * @param tname
+	 * @param type
+	 */
+	public CallTemplate(String slotname, String tname, Type type) {
+		super("RENDER.CALLTEMPLATE");
+		setSlotname(slotname);
+		setTname(tname);
+		setTtype(type);
+		setStyle(Style.element);
+		setContext("");
 
-    public void setTtype(String s) { set("TTYPE", s); }
+	}
 
-    public void setC(String s) { set("C", s); }
+	public void setSite(String s) {
+		set("SITE", s);
+	}
 
-    public void setCid(String s) { set("CID", s); }
+	public void setSlotname(String s) {
+		set("SLOTNAME", s);
+	}
 
-    public void setTname(String s) { set("TNAME", s); }
+	public void setTid(String s) {
+		set("TID", s);
+	}
 
-    public void setContext(String s) { set("CONTEXT", s); }
+	public void setTtype(Type s) {
+		set("TTYPE", s.toString());
+	}
 
-    public void setStyle(String s) {set("STYLE", s);}
+	public void setC(String s) {
+		set("C", s);
+	}
 
-    public void setVariant(String s) { set("VARIANT", s); }
+	public void setCid(String s) {
+		set("CID", s);
+	}
 
-    public void setPackedargs(String s) { set("PACKEDARGS", s); }
+	public void setTname(String s) {
+		set("TNAME", s);
+	}
 
-    public void setAsset(AssetId id)
-    {
-        setC(id.getType());
-        setCid(Long.toString(id.getId()));
-    }
+	public void setContext(String s) {
+		set("CONTEXT", s);
+	}
 
+	public void setStyle(Style s) {
+		set("STYLE", s.toString());
+	}
+
+	public void setVariant(String s) {
+		set("VARIANT", s);
+	}
+
+	public void setPackedargs(String s) {
+		set("PACKEDARGS", s);
+	}
+
+	public void setAsset(AssetId id) {
+		setC(id.getType());
+		setCid(Long.toString(id.getId()));
+	}
 
 }
