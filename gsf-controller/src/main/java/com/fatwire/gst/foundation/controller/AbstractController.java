daa1ccbb6e8635d38e05e4374d00f47e05a8d2d8
Tony Field <Tony.Field@metastratus.com>
2010-06-17 03:42:28 +0000

    More enhancements

--- gsf-controller/src/main/java/com/fatwire/gst/foundation/controller/AbstractController.java
+++ gsf-controller/src/main/java/com/fatwire/gst/foundation/controller/AbstractController.java
@@ -1,28 +1,25 @@
 package com.fatwire.gst.foundation.controller;
 
-import static COM.FutureTense.Interfaces.Utilities.goodString;
-
+import COM.FutureTense.Interfaces.*;
+import COM.FutureTense.Util.ftErrors;
+import COM.FutureTense.XML.Template.Seed2;
+import com.fatwire.gst.foundation.CSRuntimeException;
+import com.openmarket.xcelerate.publish.Render;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import COM.FutureTense.Interfaces.FTValList;
-import COM.FutureTense.Interfaces.ICS;
-import COM.FutureTense.Interfaces.IPS;
-import COM.FutureTense.XML.Template.Seed2;
-
-import com.fatwire.gst.foundation.CSRuntimeException;
-import com.openmarket.xcelerate.publish.Render;
+import static COM.FutureTense.Interfaces.Utilities.goodString;
 
 /**
- * 
  * @author Tony Field
  * @author Dolf.Dijkstra
  * @since Jun 16, 2010
  */
 
-public abstract class AbstractController implements Seed2 {
-    protected static final Log LOG = LogFactory
-            .getLog("com.fatwire.gst.foundation.controller");
+public abstract class
+        AbstractController implements Seed2
+{
+    protected static final Log LOG = LogFactory.getLog("com.fatwire.gst.foundation.controller");
 
     public static String STATUS_HEADER = "X-Fatwire-Status";
 
@@ -32,7 +29,9 @@ public abstract class AbstractController implements Seed2 {
      * (non-Javadoc)
      * @see COM.FutureTense.XML.Template.Seed2#SetAppLogic(COM.FutureTense.Interfaces.IPS)
      */
-    public void SetAppLogic(final IPS ips) {
+
+    public void SetAppLogic(final IPS ips)
+    {
         ics = ips.GetICSObject();
 
     }
@@ -41,10 +40,15 @@ public abstract class AbstractController implements Seed2 {
      * (non-Javadoc)
      * @see COM.FutureTense.XML.Template.Seed#Execute(COM.FutureTense.Interfaces.FTValList, COM.FutureTense.Interfaces.FTValList)
      */
-    public final String Execute(final FTValList vIn, final FTValList vOut) {
-        try {
+
+    public final String Execute(final FTValList vIn, final FTValList vOut)
+    {
+        try
+        {
             doExecute();
-        } catch (final Exception e) {
+        }
+        catch(final Exception e)
+        {
             handleException(e);
         }
 
@@ -53,75 +57,79 @@ public abstract class AbstractController implements Seed2 {
 
     /**
      * Sends the http status code to the user-agent
-     * 
+     *
      * @param code the http response code
      * @return String to stream
      */
-    protected String sendError(final int code, final Exception e) {
-        LOG.debug(code + " status code sent due to exception " + e.toString(),
-                e);
-        switch (code) { //all the http status codes, we may restrict the list to error and redirect
-        case 100:
-        case 101:
-        case 200:
-        case 201:
-        case 202:
-        case 203:
-        case 204:
-        case 205:
-        case 206:
-        case 300:
-        case 301:
-        case 302:
-        case 303:
-        case 304:
-        case 306:
-        case 307:
-        case 400:
-        case 401:
-        case 402:
-        case 403:
-        case 404:
-        case 405:
-        case 406:
-        case 407:
-        case 408:
-        case 409:
-        case 410:
-        case 411:
-        case 412:
-        case 413:
-        case 414:
-        case 415:
-        case 416:
-        case 417:
-        case 450:
-        case 500:
-        case 501:
-        case 502:
-        case 503:
-        case 504:
-        case 505:
-            ics.StreamHeader(STATUS_HEADER, Integer.toString(code));
-            break;
-        default:
-            ics.StreamHeader(STATUS_HEADER, "500");
-            break;
+    protected String sendError(final int code, final Exception e)
+    {
+        LOG.debug(code + " status code sent due to exception " + e.toString(), e);
+        switch(code)
+        { //all the http status codes, we may restrict the list to error and redirect
+            case 100:
+            case 101:
+            case 200:
+            case 201:
+            case 202:
+            case 203:
+            case 204:
+            case 205:
+            case 206:
+            case 300:
+            case 301:
+            case 302:
+            case 303:
+            case 304:
+            case 306:
+            case 307:
+            case 400:
+            case 401:
+            case 402:
+            case 403:
+            case 404:
+            case 405:
+            case 406:
+            case 407:
+            case 408:
+            case 409:
+            case 410:
+            case 411:
+            case 412:
+            case 413:
+            case 414:
+            case 415:
+            case 416:
+            case 417:
+            case 450:
+            case 500:
+            case 501:
+            case 502:
+            case 503:
+            case 504:
+            case 505:
+                ics.StreamHeader(STATUS_HEADER, Integer.toString(code));
+                break;
+            default:
+                ics.StreamHeader(STATUS_HEADER, "500");
+                break;
         }
         Render.UnknownDeps(ics);//failure case might be corrected on next publish or save
         String element = null;
 
-        if (goodString(ics.GetVar("site"))
-                && ics.IsElement(ics.GetVar("site") + "/ErrorHandler/" + code)) {
+        if(goodString(ics.GetVar("site")) && ics.IsElement(ics.GetVar("site") + "/ErrorHandler/" + code))
+        {
             element = ics.GetVar("site") + "/ErrorHandler/" + code;
-        } else if (ics.IsElement("GST/ErrorHandler/" + code)) {
+        }
+        else if(ics.IsElement("GST/ErrorHandler/" + code))
+        {
             element = "GST/ErrorHandler/" + code;
         }
-        if (element != null) {
+        if(element != null)
+        {
             ics.SetObj("com.fatwire.gst.foundation.exception", e);
             ics.CallElement(element, null);
         }
-        ics.SetErrno(-500); // or whatever make sense
+        ics.SetErrno(ftErrors.exceptionerr);
 
         return null;
 
@@ -130,7 +138,6 @@ public abstract class AbstractController implements Seed2 {
     /**
      * Executes the core business logic of the controller.
      *
-
      * @throws CSRuntimeException may throw a CSRuntimeException which is handled by handleCSRuntimeException
      */
     abstract protected void doExecute();
