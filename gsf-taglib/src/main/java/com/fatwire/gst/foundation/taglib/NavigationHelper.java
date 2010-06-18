51e09b9bb17c575b708b7e4c83c13212654dfea8
Dolf Dijkstra <Dolf.Dijkstra@xs4all.nl>
2010-06-18 12:54:04 +0000

    Fixing javadoc and making ics private

--- gsf-taglib/src/main/java/com/fatwire/gst/foundation/taglib/NavigationHelper.java
+++ gsf-taglib/src/main/java/com/fatwire/gst/foundation/taglib/NavigationHelper.java
@@ -17,19 +17,19 @@ import com.fatwire.gst.foundation.facade.runtag.render.GetTemplateUrl;
 import com.fatwire.gst.foundation.facade.runtag.siteplan.ListPages;
 
 /**
- * Used to retrieve the Navigation Bar data. See the description of getSitePlanAsMap(String pageid) for more details.
+ * Used to retrieve the Navigation Bar data. See the description of
+ * getSitePlanAsMap(String pageid) for more details.
  * <p/>
- * Created by IntelliJ IDEA.
- * User: David Chesebro
- * Date: Jun 17, 2010
- * Time: 2:31:27 PM
+ * 
+ * @author David Chesebro
+ * @since Jun 17, 2010
  */
 public final class NavigationHelper {
     private final ICS ics;
 
     /**
      * Constructor
-     *
+     * 
      * @param ics object
      */
     public NavigationHelper(ICS ics) {
@@ -37,29 +37,34 @@ public final class NavigationHelper {
     }
 
     /**
-     * Name of the page subtype indicating that this page is NOT rendered on the site but
-     * is instead merely used to group navigation components on the site.
+     * Name of the page subtype indicating that this page is NOT rendered on the
+     * site but is instead merely used to group navigation components on the
+     * site.
      */
     public static final String NAVBAR_NAME = "GSTNavName";
 
     /**
-     * Get a Map<String,Object> object of the site plan tree containing all the attributes necessary to create a nav bar.
-     * The Map contains the following keys:
-     * - url: String containing the href for the page (as defined in the GST Site Foundation design doc)
-     * - linktext: String containing the text to be used to represent the link. Uses the "linktext" attribute of the page's
-     * asset, or if that's empty use the "h1text" attribute
-     * - pageid: id of the page
-     * - subtype: subtype of the page
-     * - level: the number of levels down the site plan tree of the page asset (starting with the pageid you originally pass in = level 0)
-     * - children (a List<Map<String,Object>> of the children (in the site plan tree) of the page, where each Map
-     * contains the above attributes
+     * Get a Map<String,Object> object of the site plan tree containing all the
+     * attributes necessary to create a nav bar. The Map contains the following
+     * keys: - url: String containing the href for the page (as defined in the
+     * GST Site Foundation design doc) - linktext: String containing the text to
+     * be used to represent the link. Uses the "linktext" attribute of the
+     * page's asset, or if that's empty use the "h1text" attribute - pageid: id
+     * of the page - subtype: subtype of the page - level: the number of levels
+     * down the site plan tree of the page asset (starting with the pageid you
+     * originally pass in = level 0) - children (a List<Map<String,Object>> of
+     * the children (in the site plan tree) of the page, where each Map contains
+     * the above attributes
      * <p/>
-     * Links are not populated for Navigation Placeholders, but it is often very convenient to pass a navigation
-     * placeholder into this function in order to return all children under a specific placeholder.
+     * Links are not populated for Navigation Placeholders, but it is often very
+     * convenient to pass a navigation placeholder into this function in order
+     * to return all children under a specific placeholder.
      * <p/>
      * Aliases are resolved.
-     *
-     * @param pageid AssetId of (usually a page) in the site plan tree to start with.  Typically this would be a nav name. The nav name would be included in the output object. Recursion is automatic
+     * 
+     * @param pageid AssetId of (usually a page) in the site plan tree to start
+     *            with. Typically this would be a nav name. The nav name would
+     *            be included in the output object. Recursion is automatic
      * @return Map<String,Object> of the site plan tree (see above)
      */
     public Map<String, Object> getSitePlanAsMap(String pageid) {
@@ -67,10 +72,11 @@ public final class NavigationHelper {
     }
 
     /**
-     * Called from public Map<String,Object> getSitePlanAsMap(String pageid). See that function's description for details
-     *
+     * Called from public Map<String,Object> getSitePlanAsMap(String pageid).
+     * See that function's description for details
+     * 
      * @param pageid id of the page assest
-     * @param level  starting level number when traversing the site plan tree
+     * @param level starting level number when traversing the site plan tree
      * @return Map<String,Object> of the site plan tree
      */
     private Map<String, Object> getSitePlanAsMap(String pageid, int level) {
@@ -79,13 +85,15 @@ public final class NavigationHelper {
         final boolean isNavigationPlaceholder = NAVBAR_NAME.equals(subtype);
         String linkText = null;
         String url = null;
-        // get the link text from the "linktext" attribute of the asset in the page's unnamed association
+        // get the link text from the "linktext" attribute of the asset in the
+        // page's unnamed association
         if (!isNavigationPlaceholder) {
             AssetId pageAssetId = Children.getSingleAssociation(ics, "Page", pageid, "-");
             AssetData pageAssetData = AssetDataUtils.getAssetData(pageAssetId, "linktext", "h1title", "template");
             linkText = AttributeDataUtils.getWithFallback(pageAssetData, "linktext", "h1title");
             String tname = pageAssetData.getAttributeData("template").getData().toString();
-            String wrapper = ics.GetProperty("com.fatwire.gst.foundation.url.wrapathassembler.dispatcher", "ServletRequest.properties", true);
+            String wrapper = ics.GetProperty("com.fatwire.gst.foundation.url.wrapathassembler.dispatcher",
+                    "ServletRequest.properties", true);
             if (!Utilities.goodString(wrapper)) {
                 wrapper = "GSF/Dispatcher";
             }
@@ -103,12 +111,13 @@ public final class NavigationHelper {
     }
 
     /**
-     * runs the GetTemplateUrl and with the given attributes and returns the resulting url as a String
-     *
-     * @param c        asset type
-     * @param cid      asset id
-     * @param tname    template name
-     * @param wrapper  wrapper name
+     * runs the GetTemplateUrl and with the given attributes and returns the
+     * resulting url as a String
+     * 
+     * @param c asset type
+     * @param cid asset id
+     * @param tname template name
+     * @param wrapper wrapper name
      * @param slotname slot name
      * @return String containing the resulting href
      */
@@ -123,36 +132,46 @@ public final class NavigationHelper {
     }
 
     /**
-     * Get a PageLinkData object from the site plan tree.  If the linktext is derived from an associated asset,
-     * specify the association name.  If the linktet is derived from the page asset itself, leave detailAssocName
-     * as null.  Either way, the linktext comes from the appropriate asset's linktextAttr.
+     * Get a PageLinkData object from the site plan tree. If the linktext is
+     * derived from an associated asset, specify the association name. If the
+     * linktet is derived from the page asset itself, leave detailAssocName as
+     * null. Either way, the linktext comes from the appropriate asset's
+     * linktextAttr.
      * <p/>
-     * Links are not populated for Navigation Placeholders, but it is often very convenient to pass a navigation
-     * placeholder into this function in order to return all children under a specific placeholder.
+     * Links are not populated for Navigation Placeholders, but it is often very
+     * convenient to pass a navigation placeholder into this function in order
+     * to return all children under a specific placeholder.
      * <p/>
      * Aliases are resolved.
-     *
-     * @param id                AssetId of (usually a page) in teh site plan tree to start with.  Typically this would be a nav name. The nav name would be included in the output object. Recursion is automatic
-     * @param dimensionSetId    DimensionSet id
-     * @param preferredLocaleid ID of locale asset to use to calculate the links for the navigation.  Null is allowed but only if locale is not used (obviously)
+     * 
+     * @param id AssetId of (usually a page) in teh site plan tree to start
+     *            with. Typically this would be a nav name. The nav name would
+     *            be included in the output object. Recursion is automatic
+     * @param dimensionSetId DimensionSet id
+     * @param preferredLocaleid ID of locale asset to use to calculate the links
+     *            for the navigation. Null is allowed but only if locale is not
+     *            used (obviously)
      * @return PageLinkData
      */
     public Map<String, Object> getSitePlanAsMap(String id, String dimensionSetId, String preferredLocaleid) {
-        return getSitePlanAsMap(id);  //TODO: use dimensionSetId and preferredLocaleid instead of calling this
+        return getSitePlanAsMap(id); // TODO: use dimensionSetId and
+        // preferredLocaleid instead of calling
+        // this
     }
 
     /**
      * Puts the page attributes into a map
-     *
-     * @param URL      href
+     * 
+     * @param URL href
      * @param linkText link text
-     * @param pageId   page id
+     * @param pageId page id
      * @param children list of children pages in site plan
-     * @param subtype  page subtype
-     * @param level    level in the site plan tree
+     * @param subtype page subtype
+     * @param level level in the site plan tree
      * @return Map containing these attributes
      */
-    private Map<String, Object> createPageLink(String URL, String linkText, String pageId, List<Map<String, Object>> children, String subtype, int level) {
+    private Map<String, Object> createPageLink(String URL, String linkText, String pageId,
+            List<Map<String, Object>> children, String subtype, int level) {
         Map<String, Object> pageLink = new HashMap<String, Object>();
 
         pageLink.put("url", URL);
--- gsf-taglib/src/main/java/com/fatwire/gst/foundation/taglib/WRAUtils.java
+++ gsf-taglib/src/main/java/com/fatwire/gst/foundation/taglib/WRAUtils.java
@@ -10,31 +10,31 @@ import com.fatwire.gst.foundation.facade.assetapi.AssetDataUtils;
 import com.fatwire.gst.foundation.facade.assetapi.AttributeDataUtils;
 
 /**
- * Created by IntelliJ IDEA.
- * User: David Chesebro
- * Date: Jun 17, 2010
- * Time: 2:31:17 PM
+ * @author David Chesebro
+ * @since Jun 17, 2010
  */
 public class WRAUtils {
-    ICS ics;
+    private ICS ics;
 
     public WRAUtils(ICS ics) {
         this.ics = ics;
     }
 
     /**
-     * Return a Map<String,String> containing the common information found inside a HEAD html tag.
+     * Return a Map<String,String> containing the common information found
+     * inside a HEAD html tag.
      * <p/>
      * Does not resolve aliases.
-     *
-     * @param c   asset type
+     * 
+     * @param c asset type
      * @param cid asset id
      * @return Map<String,String> with keys title, keywords, and description
      */
     public Map<String, String> getCoreFields(String c, String cid) {
         Map<String, String> coreFields = new HashMap<String, String>();
 
-        AssetData data = AssetDataUtils.getAssetData(c, cid, "metatitle", "metadescription", "metakeyword", "h1title", "linktext", "path", "template");
+        AssetData data = AssetDataUtils.getAssetData(c, cid, "metatitle", "metadescription", "metakeyword", "h1title",
+                "linktext", "path", "template");
         String metatitle = data.getAttributeData("metatitle").getData().toString();
         String metakeyword = AttributeDataUtils.getMultivaluedAsCommaSepString(data.getAttributeData("metakeyword"));
         String metadescription = data.getAttributeData("metadescription").getData().toString();
