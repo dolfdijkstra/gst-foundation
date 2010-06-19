9a586b6318c9663a706bbca49f336e13cba51ac5
Tony Field <Tony.Field@metastratus.com>
2010-06-19 01:18:54 +0000

    added method to split a string attr value

--- gsf-facades/src/main/java/com/fatwire/gst/foundation/facade/assetapi/AttributeDataUtils.java
+++ gsf-facades/src/main/java/com/fatwire/gst/foundation/facade/assetapi/AttributeDataUtils.java
@@ -8,10 +8,12 @@
  */
 package com.fatwire.gst.foundation.facade.assetapi;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
 import com.fatwire.assetapi.data.AssetData;
 import com.fatwire.assetapi.data.AttributeData;
-
-import java.util.Arrays;
 
 /**
  * Utility class for processing attribute data
@@ -19,9 +21,9 @@ import java.util.Arrays;
  * @author Tony Field
  * @since Nov 17, 2009
  */
-public final class AttributeDataUtils
-{
-    private AttributeDataUtils() {}
+public final class AttributeDataUtils {
+    private AttributeDataUtils() {
+    }
 
     /**
      * Get the specified attribute field from the AssetData object.  If it is not found,
@@ -29,18 +31,15 @@ public final class AttributeDataUtils
      * a single value.
      * If no data is found for any attribute, an exception is thrown.
      *
-     * @param assetData populated AssetData object
+     * @param assetData             populated AssetData object
      * @param orderedAttributeNames vararg array of attribute names that are expected to be found and populated in
-     * the assetData object
+     *                              the assetData object
      * @return the value, never null
      */
-    public static String getWithFallback(AssetData assetData, String... orderedAttributeNames)
-    {
-        for(String attr : orderedAttributeNames)
-        {
+    public static String getWithFallback(AssetData assetData, String... orderedAttributeNames) {
+        for (String attr : orderedAttributeNames) {
             AttributeData attrData = assetData.getAttributeData(attr);
-            if(attrData.getData() != null)
-            {
+            if (attrData.getData() != null) {
                 return attrData.getData().toString();
             }
         }
@@ -54,17 +53,31 @@ public final class AttributeDataUtils
      * @param attributeData multi-valued attribute data
      * @return attribute data converted to a string, comma-separated.
      */
-    public static String getMultivaluedAsCommaSepString(AttributeData attributeData)
-    {
+    public static String getMultivaluedAsCommaSepString(AttributeData attributeData) {
         StringBuilder sb = new StringBuilder();
-        for(Object vals : attributeData.getDataAsList())
-        {
-            if(sb.length() > 0)
-            {
+        for (Object vals : attributeData.getDataAsList()) {
+            if (sb.length() > 0) {
                 sb.append(",");
             }
             sb.append(vals.toString());
         }
         return sb.toString();
+    }
+
+    /**
+     * Get an attribute that is a comma-separated string and split it into
+     * a collection.  If the attribute has no values, an empty list is returned.
+     *
+     * @param attributeData
+     * @param delimRegex    regex for splitting
+     * @return Collection<String> of attribute data, never null (though an empty list may be returned
+     */
+    public static Collection<String> getAndSplitString(AttributeData attributeData, String delimRegex) {
+        if (attributeData == null) return Collections.EMPTY_LIST;
+        Object o = attributeData.getData();
+        if (o != null) {
+            String[] s = ((String) o).split(delimRegex);
+            return Arrays.asList(s);
+        } else return Collections.EMPTY_LIST;
     }
 }
