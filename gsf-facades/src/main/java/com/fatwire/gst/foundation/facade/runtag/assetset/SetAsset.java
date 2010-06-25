864d7b786cd86abadb1a8c88ff63a434c21bc724
Dolf Dijkstra <Dolf.Dijkstra@xs4all.nl>
2010-06-25 18:23:21 +0000

    Adding license header

--- gsf-facades/src/main/java/com/fatwire/gst/foundation/facade/runtag/assetset/SetAsset.java
+++ gsf-facades/src/main/java/com/fatwire/gst/foundation/facade/runtag/assetset/SetAsset.java
@@ -1,11 +1,19 @@
 /*
- * Copyright (c) 2008 FatWire Corporation. All Rights Reserved.
- * Title, ownership rights, and intellectual property rights in and
- * to this software remain with FatWire Corporation. This  software
- * is protected by international copyright laws and treaties, and
- * may be protected by other law.  Violation of copyright laws may
- * result in civil liability and criminal penalties.
+ * Copyright 2008 FatWire Corporation. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 package com.fatwire.gst.foundation.facade.runtag.assetset;
 
 import com.fatwire.gst.foundation.facade.runtag.AbstractTagRunner;
