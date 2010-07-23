977e60be31b5cf7e52fa11effd88c7a56630ae61
Tony Field <Tony.Field@metastratus.com>
2010-07-23 02:01:01 +0000

    Completed implementation of UrlRegistry table and WraPageReference.  This has not yet been tested.

--- gsf-controller/src/main/java/com/fatwire/gst/foundation/url/UrlRegistry.java
+++ gsf-controller/src/main/java/com/fatwire/gst/foundation/url/UrlRegistry.java
@@ -17,6 +17,8 @@ import com.fatwire.gst.foundation.facade.sql.table.TableColumn;
 import com.fatwire.gst.foundation.facade.sql.table.TableColumn.Type;
 import com.fatwire.gst.foundation.facade.sql.table.TableCreator;
 import com.fatwire.gst.foundation.facade.sql.table.TableDef;
+import com.fatwire.gst.foundation.facade.vwebroot.VirtualWebroot;
+import com.fatwire.gst.foundation.facade.vwebroot.VirtualWebrootDao;
 import com.fatwire.gst.foundation.facade.wra.WebReferenceableAsset;
 import com.fatwire.gst.foundation.facade.wra.WraCoreFieldDao;
 
@@ -34,11 +36,13 @@ public class UrlRegistry implements WraPathTranslationService {
 
     private final ICS ics;
     private final WraCoreFieldDao wraDao;
+    private final VirtualWebrootDao vwDao;
     private static final String URLREG_TABLE = "GSTUrlRegistry";
 
     UrlRegistry(ICS ics) {
         this.ics = ics;
         wraDao = new WraCoreFieldDao();
+        vwDao = new VirtualWebrootDao(ics);
     }
 
     public void install() {
@@ -109,9 +113,10 @@ public class UrlRegistry implements WraPathTranslationService {
         long assetid = asset.getId();
         Date start = wra.getStartDate();
         Date end = wra.getEndDate();
-        String vwebroot = "http://www.fatwire.com"; // todo: implement
-        String urlpath = "/products/cs"; // todo: implement
-        int depth = 2; // todo: implement
+        VirtualWebroot vw = vwDao.lookupVirtualWebrootForAsset(wra);
+        String vwebroot = vw.getEnvironmentVirtualWebroot();
+        String urlpath = wra.getPath().substring(vw.getMasterVirtualWebroot().length() + 1);
+        int depth = urlpath != null && urlpath.length() > 0 ? urlpath.split("/").length : 0;
         String site = wraDao.resolveSite(asset.getType(), Long.toString(asset.getId()));
 
         String qry = "insert into " + URLREG_TABLE + " (id, path, assettype, assetid, " + (start == null ? "" : "startdate, ") + (end == null ? "" : "enddate,") + " opt_vwebroot, opt_url_path, opt_depth, opt_site)" + " VALUES " + "(" + id + "," + quote(path) + "," + quote(assettype) + "," + assetid + "," + (start == null ? "" : quote(Util.formatJdbcDate(start)) + ",") + (end == null ? "" : quote(Util.formatJdbcDate(end)) + ",") + quote(vwebroot) + "," + quote(urlpath) + "," + depth + "," + quote(site) + ")";
@@ -127,6 +132,4 @@ public class UrlRegistry implements WraPathTranslationService {
     public void deleteAsset(AssetId id) {
         SqlHelper.execute(ics, URLREG_TABLE, "delete from " + URLREG_TABLE + " where assettype = '" + id.getType() + "' and assetid = " + id.getId());
     }
-
-
 }
