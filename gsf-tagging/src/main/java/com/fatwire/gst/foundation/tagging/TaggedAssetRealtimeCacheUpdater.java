a28ed334517072ebb2791d42c8660ee0e9daa490
Dolf Dijkstra <Dolf.Dijkstra@xs4all.nl>
2010-08-10 08:37:55 +0000

    Adding debug info for traceablility

--- gsf-tagging/src/main/java/com/fatwire/gst/foundation/tagging/TaggedAssetRealtimeCacheUpdater.java
+++ gsf-tagging/src/main/java/com/fatwire/gst/foundation/tagging/TaggedAssetRealtimeCacheUpdater.java
@@ -28,15 +28,20 @@ import org.apache.commons.logging.LogFactory;
 import static com.fatwire.gst.foundation.tagging.TagUtils.convertTagToCacheDepString;
 
 /**
- * RealTime publishing includes an API entitled RealTime CacheUpdater. We will override the default
- * com.fatwire.realtime.PageCacheUpdaterImpl to override the beforeSelect() method. The flush and regen keys will be
- * extended, to automatically include all pagelets containing the GSTTag attribute value. This ensures that even though
- * parent has not changed, pagelets that reference it are automatically flushed. This ensures that by simply tagging an
- * asset, it automatically and instantly appears in pages that render it.
- * The GSTTagRegistry table is read for the specific assets before the new values are inserted. This is to make sure that
- * pagelets are also flushed with the old tag values for the cases where the tag is deleted or the values have changed.
- * Implementation examples can be found in the guide Customizing RealTime Publishing Cache Management. 
- *
+ * RealTime publishing includes an API entitled RealTime CacheUpdater. We will
+ * override the default com.fatwire.realtime.PageCacheUpdaterImpl to override
+ * the beforeSelect() method. The flush and regen keys will be extended, to
+ * automatically include all pagelets containing the GSTTag attribute value.
+ * This ensures that even though parent has not changed, pagelets that reference
+ * it are automatically flushed. This ensures that by simply tagging an asset,
+ * it automatically and instantly appears in pages that render it.
+ * <p/>
+ * The GSTTagRegistry table is read for the specific assets before the new
+ * values are inserted. This is to make sure that pagelets are also flushed with
+ * the old tag values for the cases where the tag is deleted or the values have
+ * changed. Implementation examples can be found in the guide Customizing
+ * RealTime Publishing Cache Management.
+ * 
  * @author Tony Field
  * @since Jul 30, 2010
  */
@@ -45,16 +50,19 @@ public final class TaggedAssetRealtimeCacheUpdater extends PageCacheUpdaterImpl 
     private static final Log LOG = LogFactory.getLog("com.fatwire.gst.foundation.tagging");
 
     @Override
-    protected void beforeSelect(ICS ics, Collection<String> strings, Collection<String> strings1, Collection<AssetId> assetIds) {
+    protected void beforeSelect(ICS ics, Collection<String> strings, Collection<String> strings1,
+            Collection<AssetId> assetIds) {
         AssetTaggingService svc = AssetTaggingServiceFactory.getService(ics);
-        Collection<Tag> tags = svc.getTags(assetIds);
         for (Tag tag : svc.getTags(assetIds)) {
+            if (LOG.isDebugEnabled())
+                LOG.debug("AssetTag found in beforeSelect: " + tag
+                        + ". Adding this to the list of compositional dependencies to be flushed.");
             String sTag = convertTagToCacheDepString(tag);
-            // todo: ensure this is right.... I THINK it's flushStrings & refreshStrings... and if so, we may not need to refresh.
+            // todo: ensure this is right.... I THINK it's flushStrings &
+            // refreshStrings... and if so, we may not need to refresh.
             strings.add(sTag);
             strings1.add(sTag);
         }
         super.beforeSelect(ics, strings, strings1, assetIds);
     }
 }
-
