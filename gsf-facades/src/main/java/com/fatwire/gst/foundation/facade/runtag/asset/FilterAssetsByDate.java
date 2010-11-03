c7d403fb6e971b0b89bee778ad0024dcda1e7e3d
Tony Field <Tony.Field@metastratus.com>
2010-11-03 21:48:09 +0000

    Better date filtering.

--- gsf-facades/src/main/java/com/fatwire/gst/foundation/facade/runtag/asset/FilterAssetsByDate.java
+++ gsf-facades/src/main/java/com/fatwire/gst/foundation/facade/runtag/asset/FilterAssetsByDate.java
@@ -17,16 +17,22 @@
 package com.fatwire.gst.foundation.facade.runtag.asset;
 
 import java.text.ParseException;
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
 
 import COM.FutureTense.Interfaces.ICS;
+import COM.FutureTense.Interfaces.IList;
 import COM.FutureTense.Interfaces.Utilities;
 import COM.FutureTense.Util.ftMessage;
 
 import com.fatwire.assetapi.data.AssetData;
 import com.fatwire.assetapi.data.AssetId;
+import com.fatwire.gst.foundation.IListUtils;
 import com.fatwire.gst.foundation.facade.assetapi.AssetDataUtils;
+import com.fatwire.gst.foundation.facade.assetapi.AssetIdIList;
 import com.fatwire.gst.foundation.facade.assetapi.AttributeDataUtils;
+import com.fatwire.gst.foundation.facade.runtag.AbstractTagRunner;
 
 import org.apache.commons.lang.time.DateUtils;
 import org.apache.commons.logging.Log;
@@ -37,19 +43,64 @@ import static COM.FutureTense.Interfaces.Utilities.goodString;
 /**
  * Filters assets via startdate/enddate.
  * <p/>
- * This class is not yet complete and offers only basic functionality.
  * <p/>
- * TODO: implement core functionality for ASSET:FILTERASSETSBYDATE
  *
  * @author Tony Field
  * @since Jun 23, 2010
  */
-public final class FilterAssetsByDate {
+public final class FilterAssetsByDate extends AbstractTagRunner {
     private static final Log LOG = LogFactory.getLog(FilterAssetsByDate.class);
     private static final String STARTDATE = "startdate";
     private static final String ENDDATE = "enddate";
 
     private static String[] jdbcDateFormatStrings = {"yyyy-MM-dd HH:mm:ss", "yyyy-MM-dd HH:mm:ss.SSS"};
+
+
+    public FilterAssetsByDate() {
+        super("ASSET.FILTERASSETSBYDATE");
+    }
+
+    public void setInputList(String s) {
+        set("INPUTLIST", s);
+    }
+
+    public void setOutputList(String s) {
+        set("OUTPUTLIST", s);
+    }
+
+    public void setDate(String s) {
+        set("DATE", s);
+    }
+
+    public void setInputList(ICS ics, List<AssetId> ids) {
+        final String name = "FilterAssetByDateInputList-" + ics.genID(false);
+        IList list = new AssetIdIList(name, ids);
+        ics.RegisterList(name, list);
+        setInputList(name);
+    }
+
+    public void setSingleAssetInput(ICS ics, AssetId id) {
+        setInputList(ics, Collections.singletonList(id));
+    }
+
+    public static boolean filterAsset(ICS ics, AssetId input) {
+        FilterAssetsByDate tag = new FilterAssetsByDate();
+        tag.setInputList(ics, Collections.singletonList(input));
+        final String outlist = "FilterAssetsByDateOutputList-" + ics.genID(false);
+        tag.setOutputList(outlist);
+        tag.execute(ics);
+        IList out = ics.GetList(outlist);
+        if (out == null) throw new IllegalStateException("Tag executed successfully but no outlist was returned");
+        if (!out.hasData()) {
+            return false; // no matches
+        }
+        String c = IListUtils.getStringValue(out, "assettype");
+        if (!input.getType().equals(c)) {
+            return false; // wrong type - not supposed to be even possible
+        }
+        String cid = IListUtils.getStringValue(out, "assetid");
+        return Utilities.goodString(cid) && input.getId() == Long.parseLong(cid);
+    }
 
 
     public static boolean isValidOnDate(AssetId id, Date date) {
@@ -92,9 +143,9 @@ public final class FilterAssetsByDate {
             if (effectiveDate == null) effectiveDate = new Date();
 
             if (startDate == null) {
-                return effectiveDate.before(endDate);
+                return effectiveDate.before(endDate) || effectiveDate.equals(endDate);
             } else if (endDate == null) {
-                return startDate.before(effectiveDate);
+                return startDate.before(effectiveDate) || startDate.equals(effectiveDate);
             } else {
                 return startDate.before(effectiveDate) && effectiveDate.before(endDate);
             }
