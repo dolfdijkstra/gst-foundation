285d636efab84b4dcefc1ccdbfff0eb559d328d5
Dolf Dijkstra <Dolf.Dijkstra@xs4all.nl>
2011-01-27 14:30:05 +0000

    Adding HttpStatusServlet to the foundation.

---
+++ gsf-wra/src/main/java/com/fatwire/gst/foundation/httpstatus/HttpStatusStrings.java
@@ -0,0 +1,8 @@
+package com.fatwire.gst.foundation.httpstatus;
+
+public class HttpStatusStrings
+{
+	public static final String X_FATWIRE_STATUS = "X-Fatwire-Status";
+	public static final String X_FATWIRE_HEADER = "X-Fatwire-Header";
+
+}
