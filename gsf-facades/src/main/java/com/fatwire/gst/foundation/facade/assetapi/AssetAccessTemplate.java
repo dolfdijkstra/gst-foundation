30a288578188826fe1bc7d52295bebbb5ee35bd9
Dolf Dijkstra <Dolf.Dijkstra@xs4all.nl>
2011-03-11 16:02:09 +0000

    Improving javadoc.

--- gsf-facades/src/main/java/com/fatwire/gst/foundation/facade/assetapi/AssetAccessTemplate.java
+++ gsf-facades/src/main/java/com/fatwire/gst/foundation/facade/assetapi/AssetAccessTemplate.java
@@ -40,8 +40,9 @@ import com.openmarket.xcelerate.asset.AssetIdImpl;
 
 /**
  * 
- * Helper class to facilitate {@link AssetDataManager} use in a simplified way
- * in delivery ContentServer templates.
+ * This class is a one-stop-shop for all read-only access to AssetData. It acts
+ * as a helper class to facilitate {@link AssetDataManager} use in a simplified
+ * way in delivery ContentServer templates.
  * <p/>
  * This class is inspired by springframework data access template classes like
  * org.springframework.jdbc.core.JdbcTemplate.
@@ -55,14 +56,16 @@ import com.openmarket.xcelerate.asset.AssetIdImpl;
 public class AssetAccessTemplate {
 
     private final Session session;
+    private AssetDataManager assetDataManager;
 
     /**
      * @param session
      */
-    public AssetAccessTemplate(Session session) {
+    public AssetAccessTemplate(final Session session) {
         super();
-        if (session == null)
+        if (session == null) {
             throw new IllegalArgumentException("session cannot be null.");
+        }
         this.session = session;
     }
 
@@ -71,9 +74,10 @@ public class AssetAccessTemplate {
      * 
      * @param ics
      */
-    public AssetAccessTemplate(ICS ics) {
-        if (ics == null)
+    public AssetAccessTemplate(final ICS ics) {
+        if (ics == null) {
             throw new IllegalArgumentException("ics cannot be null.");
+        }
         session = SessionFactory.getSession(ics);
     }
 
@@ -84,7 +88,7 @@ public class AssetAccessTemplate {
      * @param cid
      * @return
      */
-    public AssetId createAssetId(String c, String cid) {
+    public AssetId createAssetId(final String c, final String cid) {
         return new AssetIdImpl(c, Long.parseLong(cid));
     }
 
@@ -97,16 +101,16 @@ public class AssetAccessTemplate {
      * @param mapper
      * @return
      */
-    public <T> T readAsset(AssetId id, AssetMapper<T> mapper) {
-        AssetDataManager m = getAssetDataManager();
+    public <T> T readAsset(final AssetId id, final AssetMapper<T> mapper) {
+        final AssetDataManager m = getAssetDataManager();
 
         T t = null;
         try {
-            Iterable<AssetData> assets = m.read(Arrays.asList(id));
-            for (AssetData assetData : assets) {
+            final Iterable<AssetData> assets = m.read(Arrays.asList(id));
+            for (final AssetData assetData : assets) {
                 t = mapper.map(assetData);
             }
-        } catch (AssetAccessException e) {
+        } catch (final AssetAccessException e) {
             throw new RuntimeAssetAccessException(e);
         }
         return t;
@@ -114,7 +118,7 @@ public class AssetAccessTemplate {
 
     /**
      * Method to read an asset and use the AssetMapper to transform the
-     * AssetData into another object as specified by the AssetMapper.
+     * AssetData into another object as specified by the AssetMapper interface..
      * 
      * @param <T>
      * @param c the assetType
@@ -122,7 +126,7 @@ public class AssetAccessTemplate {
      * @param mapper
      * @return
      */
-    public <T> T readAsset(String c, String cid, AssetMapper<T> mapper) {
+    public <T> T readAsset(final String c, final String cid, final AssetMapper<T> mapper) {
         return readAsset(this.createAssetId(c, cid), mapper);
     }
 
@@ -136,7 +140,7 @@ public class AssetAccessTemplate {
      * @param mapper
      * @return
      */
-    public <T> T readAsset(String c, long cid, AssetMapper<T> mapper) {
+    public <T> T readAsset(final String c, final long cid, final AssetMapper<T> mapper) {
         return readAsset(new AssetIdImpl(c, cid), mapper);
     }
 
@@ -151,14 +155,14 @@ public class AssetAccessTemplate {
      * @param attributes
      * @return
      */
-    public <T> T readAsset(AssetId id, AssetMapper<T> mapper, String[] attributes) {
-        AssetDataManager m = getAssetDataManager();
+    public <T> T readAsset(final AssetId id, final AssetMapper<T> mapper, final String[] attributes) {
+        final AssetDataManager m = getAssetDataManager();
 
         T t = null;
         try {
-            AssetData asset = m.readAttributes(id, Arrays.asList(attributes));
+            final AssetData asset = m.readAttributes(id, Arrays.asList(attributes));
             t = mapper.map(asset);
-        } catch (AssetAccessException e) {
+        } catch (final AssetAccessException e) {
             throw new RuntimeAssetAccessException(e);
         }
         return t;
@@ -171,16 +175,16 @@ public class AssetAccessTemplate {
      * @param id
      * @param closure
      */
-    public void readAsset(AssetId id, AssetClosure closure) {
-        AssetDataManager m = getAssetDataManager();
+    public void readAsset(final AssetId id, final AssetClosure closure) {
+        final AssetDataManager m = getAssetDataManager();
 
         try {
-            Iterable<AssetData> assets = m.read(Arrays.asList(id));
-            for (AssetData assetData : assets) {
+            final Iterable<AssetData> assets = m.read(Arrays.asList(id));
+            for (final AssetData assetData : assets) {
                 closure.work(assetData);
 
             }
-        } catch (AssetAccessException e) {
+        } catch (final AssetAccessException e) {
             throw new RuntimeAssetAccessException(e);
         }
 
@@ -188,7 +192,7 @@ public class AssetAccessTemplate {
 
     /**
      * Reads an asset based on the listed attribute names
-     * 
+     * <p/>
      * TODO: do we need to load the attribute values and prevent access to
      * non-listed attributes (prevent lazy loading)
      * 
@@ -198,32 +202,32 @@ public class AssetAccessTemplate {
      * 
      * 
      */
-    public AssetData readAsset(AssetId id, String... attributes) {
-        AssetDataManager m = getAssetDataManager();
+    public AssetData readAsset(final AssetId id, final String... attributes) {
+        final AssetDataManager m = getAssetDataManager();
 
         Iterable<AssetData> assets;
         try {
             assets = m.read(Arrays.asList(id));
-        } catch (AssetAccessException e) {
+        } catch (final AssetAccessException e) {
             throw new RuntimeAssetAccessException(e);
         }
-        if (assets == null)
+        if (assets == null) {
             return null;
-        if (assets instanceof List<?>) {
-            List<AssetData> x = (List<AssetData>) assets;
-            if (x.isEmpty())
-                return null;
-            else
-                return x.get(0);
         }
-        Iterator<AssetData> i = assets.iterator();
+        if (assets instanceof List<?>) {
+            final List<AssetData> x = (List<AssetData>) assets;
+            if (x.isEmpty()) {
+                return null;
+            } else {
+                return x.get(0);
+            }
+        }
+        final Iterator<AssetData> i = assets.iterator();
         if (i.hasNext()) {
             return i.next();
         }
         return null;
     }
-
-    private AssetDataManager assetDataManager;
 
     /**
      * @return
@@ -239,26 +243,28 @@ public class AssetAccessTemplate {
      * @param id
      * @return
      */
-    public AssetData readAsset(AssetId id) {
+    public AssetData readAsset(final AssetId id) {
 
-        AssetDataManager m = getAssetDataManager();
+        final AssetDataManager m = getAssetDataManager();
 
         Iterable<AssetData> assets;
         try {
             assets = m.read(Collections.singletonList(id));
-        } catch (AssetAccessException e) {
+        } catch (final AssetAccessException e) {
             throw new RuntimeAssetAccessException(e);
         }
-        if (assets == null)
+        if (assets == null) {
             return null;
-        if (assets instanceof List<?>) {
-            List<AssetData> x = (List<AssetData>) assets;
-            if (x.isEmpty())
-                return null;
-            else
-                return x.get(0);
         }
-        Iterator<AssetData> i = assets.iterator();
+        if (assets instanceof List<?>) {
+            final List<AssetData> x = (List<AssetData>) assets;
+            if (x.isEmpty()) {
+                return null;
+            } else {
+                return x.get(0);
+            }
+        }
+        final Iterator<AssetData> i = assets.iterator();
         if (i.hasNext()) {
             return i.next();
         }
@@ -266,16 +272,16 @@ public class AssetAccessTemplate {
     }
 
     /**
-     * @param q
+     * @param query
      * @return
      */
-    public Iterable<AssetData> readAssets(Query q) {
-        AssetDataManager m = getAssetDataManager();
+    public Iterable<AssetData> readAssets(final Query query) {
+        final AssetDataManager m = getAssetDataManager();
 
         Iterable<AssetData> assets;
         try {
-            assets = m.read(q);
-        } catch (AssetAccessException e) {
+            assets = m.read(query);
+        } catch (final AssetAccessException e) {
             throw new RuntimeAssetAccessException(e);
         }
 
@@ -283,17 +289,19 @@ public class AssetAccessTemplate {
     }
 
     /**
-     * Reading assets with the Query and using the mapper to transform the AssetData into another object, as specified by T.
+     * Reading assets with the Query and using the mapper to transform the
+     * AssetData into another object, as specified by T.
+     * 
      * @param <T>
-     * @param q
+     * @param query
      * @param mapper
      * @return
      */
-    public <T> Iterable<T> readAssets(Query q, AssetMapper<T> mapper) {
+    public <T> Iterable<T> readAssets(final Query query, final AssetMapper<T> mapper) {
 
-        List<T> r = new LinkedList<T>();
+        final List<T> r = new LinkedList<T>();
 
-        for (AssetData data : readAssets(q)) {
+        for (final AssetData data : readAssets(query)) {
             r.add(mapper.map(data));
         }
 
@@ -306,32 +314,37 @@ public class AssetAccessTemplate {
      * @param name
      * @return
      */
-    public AssetId findByName(ICS ics, String assetType, String name) {
+    public AssetId findByName(final ICS ics, final String assetType, final String name) {
         // TODO: name does not need to be unique, how do we handle this?
-        AssetList x = new AssetList();
+        final AssetList x = new AssetList();
         x.setType(assetType);
         x.setField("name", name);
-        x.setList("name");
+        x.setList("name__");
         x.execute(ics);
 
-        IList list = ics.GetList("name__");
+        final IList list = ics.GetList("name__");
         ics.RegisterList("name__", null);
         if (list != null && list.hasData()) {
             list.moveTo(1);
             try {
                 return new AssetIdImpl(assetType, Long.parseLong(list.getValue("id")));
-            } catch (NoSuchFieldException e) {
+            } catch (final NoSuchFieldException e) {
                 throw new RuntimeException(e);
             }
         } else {
-            // TODO: is this correct, or should we return null, searching for
-            // something that does not exists keeps many people busy and happy
-            throw new RuntimeException(assetType + " with name '" + name + "' not found.");
+            return null;
         }
 
     }
 
-    public SimpleQuery createNameQuery(String assetType, String assetName) {
+    /**
+     * Creates a Query to retrieve the asset by it's name.
+     * 
+     * @param assetType
+     * @param assetName
+     * @return
+     */
+    public SimpleQuery createNameQuery(final String assetType, final String assetName) {
         final SimpleQuery q = new SimpleQuery(assetType, null, ConditionFactory.createCondition("name",
                 OpTypeEnum.EQUALS, assetName), Arrays.asList("id"));
         q.getProperties().setIsBasicSearch(true);
