fb72668b0c94bff121f5d84f8e139be2125cb4f3
Dolf Dijkstra <Dolf.Dijkstra@xs4all.nl>
2011-03-25 19:15:47 +0000

    Adding shorthand methods for loading children of the current asset, as well as improving javadocs

--- gsf-facades/src/main/java/com/fatwire/gst/foundation/facade/assetapi/asset/ScatteredAssetAccessTemplate.java
+++ gsf-facades/src/main/java/com/fatwire/gst/foundation/facade/assetapi/asset/ScatteredAssetAccessTemplate.java
@@ -30,15 +30,28 @@ import com.fatwire.gst.foundation.facade.assetapi.AssetAccessTemplate;
 import com.fatwire.gst.foundation.facade.assetapi.AssetMapper;
 import com.fatwire.gst.foundation.facade.assetapi.QueryBuilder;
 
+import org.apache.commons.lang.StringUtils;
+
 /**
+ * This class provides simple access to AssetData. It is intended to be a
+ * one-stop-shop for all read operations on assets.
+ * <p/>
+ * In many cases it returns a {@link ScatteredAsset} for easy access to
+ * AssetData.
+ * <p/>
+ * The object has the same lifecycle as the ICS object, one per request.
+ * 
  * @author Dolf Dijkstra
  * 
  */
 public class ScatteredAssetAccessTemplate extends AssetAccessTemplate {
-    private ICS ics;
-    private AssetMapper<ScatteredAsset> mapper = new AssetMapper<ScatteredAsset>() {
+    private final ICS ics;
+    /**
+     * The asset mapper that returned a ScatteredAsset.
+     */
+    private final AssetMapper<ScatteredAsset> mapper = new AssetMapper<ScatteredAsset>() {
 
-        public ScatteredAsset map(AssetData assetData) {
+        public ScatteredAsset map(final AssetData assetData) {
             return new ScatteredAsset(assetData);
         }
 
@@ -47,7 +60,7 @@ public class ScatteredAssetAccessTemplate extends AssetAccessTemplate {
     /**
      * @param ics
      */
-    public ScatteredAssetAccessTemplate(ICS ics) {
+    public ScatteredAssetAccessTemplate(final ICS ics) {
         super(ics);
         this.ics = ics;
     }
@@ -56,30 +69,37 @@ public class ScatteredAssetAccessTemplate extends AssetAccessTemplate {
      * @param id
      * @return
      */
-    public ScatteredAsset read(AssetId id) {
+    public ScatteredAsset read(final AssetId id) {
         return this.readAsset(id, mapper);
     }
 
     /**
+     * Reads the attributes of an asset.
+     * 
      * @param id
      * @param attributes
      * @return
      */
-    public ScatteredAsset read(AssetId id, String... attributes) {
+    public ScatteredAsset read(final AssetId id, final String... attributes) {
         return this.readAsset(id, mapper, attributes);
     }
 
     /**
+     * Reads the associated assets of an asset and returns them as a
+     * ScatteredAsset. This takes care of the asset read operation of the
+     * associated assets.
+     * 
      * @param id
      * @param associationType
      * @return the assets from the associations.
      */
-    public Collection<ScatteredAsset> readAssociatedAssets(AssetId id, String associationType) {
-        Collection<AssetId> list = readAssociatedAssetIds(id, associationType);
-        if (list == null || list.isEmpty())
+    public Collection<ScatteredAsset> readAssociatedAssets(final AssetId id, final String associationType) {
+        final Collection<AssetId> list = readAssociatedAssetIds(id, associationType);
+        if (list == null || list.isEmpty()) {
             return Collections.emptyList();
-        List<ScatteredAsset> l = new LinkedList<ScatteredAsset>();
-        for (AssetId child : list) {
+        }
+        final List<ScatteredAsset> l = new LinkedList<ScatteredAsset>();
+        for (final AssetId child : list) {
             l.add(read(child));
         }
         return l;
@@ -87,31 +107,52 @@ public class ScatteredAssetAccessTemplate extends AssetAccessTemplate {
     }
 
     /**
+     * Reads the associated assets of the asset and returns the AssetIds.
+     * 
      * @param id
      * @param associationType
      * @return the assets from the associations.
      */
-    public Collection<AssetId> readAssociatedAssetIds(AssetId id, String associationType) {
-        List<AssetId> list = this.readAsset(id).getAssociatedAssets(associationType);
-        if (list == null)
+    public Collection<AssetId> readAssociatedAssetIds(final AssetId id, final String associationType) {
+        final List<AssetId> list = this.readAsset(id).getAssociatedAssets(associationType);
+        if (list == null) {
             return Collections.emptyList();
+        }
         return list;
 
     }
 
     /**
+     * Reads the list of associated assets based on the current c and cid cs
+     * variables.
+     * 
+     * @param associationType
+     * @return the assets from the associations.
+     */
+    public Collection<AssetId> readAssociatedAssetIds(final String associationType) {
+        return readAssociatedAssetIds(currentId(), associationType);
+    }
+
+    /**
+     * Reads the associated assets of an asset and returns them as a
+     * ScatteredAsset. This takes care of the asset read operation of the
+     * associated assets. The returned ScatteredAssets are only loaded with the
+     * mentioned attributes.
+     * 
      * @param id the parent asset
      * @param associationType the name of the association or '-' for an unnamed
      *            association
      * @param attributes the list of attributes to load
      * @return the assets from the associations.
      */
-    public Collection<ScatteredAsset> readAssociatedAssets(AssetId id, String associationType, String... attributes) {
-        List<AssetId> list = this.readAsset(id).getAssociatedAssets(associationType);
-        if (list == null || list.isEmpty())
+    public Collection<ScatteredAsset> readAssociatedAssets(final AssetId id, final String associationType,
+            final String... attributes) {
+        final List<AssetId> list = this.readAsset(id).getAssociatedAssets(associationType);
+        if (list == null || list.isEmpty()) {
             return Collections.emptyList();
-        List<ScatteredAsset> l = new LinkedList<ScatteredAsset>();
-        for (AssetId child : list) {
+        }
+        final List<ScatteredAsset> l = new LinkedList<ScatteredAsset>();
+        for (final AssetId child : list) {
             l.add(read(child, attributes));
         }
         return l;
@@ -119,29 +160,47 @@ public class ScatteredAssetAccessTemplate extends AssetAccessTemplate {
     }
 
     /**
+     * Reads the asset attributes of the asset identified by the current c and
+     * cid variables on the ics scope.
+     * 
      * @param attributes
      * @return the ScatteredAsset
      */
     public ScatteredAsset readCurrent() {
-        AssetId id = createAssetId(ics.GetVar("c"), ics.GetVar("cid"));
+        final AssetId id = currentId();
         return this.read(id);
     }
 
+    public AssetId currentId() {
+        final String c = ics.GetVar("c");
+        final String cid = ics.GetVar("cid");
+        if (StringUtils.isBlank(c) || StringUtils.isBlank(cid)) {
+            throw new IllegalStateException("c or cid is not set as a ics variable.");
+        }
+        final AssetId id = createAssetId(c, cid);
+        return id;
+    }
+
     /**
+     * Reads the mentioned asset attributes of the asset identified by the
+     * current c and cid variables on the ics scope.
+     * 
      * @param attributes
      * @return the ScatteredAsset
      */
-    public ScatteredAsset readCurrent(String... attributes) {
-        AssetId id = createAssetId(ics.GetVar("c"), ics.GetVar("cid"));
+    public ScatteredAsset readCurrent(final String... attributes) {
+        final AssetId id = currentId();
         return this.readAsset(id, mapper, attributes);
     }
 
     /**
+     * Queries the asset repository with the provided query.
+     * 
      * @param query
      * @return
      */
-    public Iterable<ScatteredAsset> query(Query query) {
-        return this.readAssets(query, mapper);
+    public Iterable<ScatteredAsset> query(final Query query) {
+        return readAssets(query, mapper);
     }
 
     /**
@@ -160,13 +219,25 @@ public class ScatteredAssetAccessTemplate extends AssetAccessTemplate {
      * @param query
      * @return a list of scattered assets.
      */
-    public Iterable<ScatteredAsset> query(String assetType, String subType, String query) {
-        Query q = new QueryBuilder(assetType, subType).condition(query).setReadAll(true).toQuery();
+    public Iterable<ScatteredAsset> query(final String assetType, final String subType, final String query) {
+        final Query q = new QueryBuilder(assetType, subType).condition(query).setReadAll(true).toQuery();
         return this.readAssets(q, mapper);
     }
 
-    public Iterable<ScatteredAsset> query(String assetType, String subType, String query, String[] attributes) {
-        Query q = new QueryBuilder(assetType, subType).condition(query).attributes(attributes).toQuery();
+    /**
+     * 
+     * Queries for a list of scattered assets. Only the mentioned attributes are
+     * returned.
+     * 
+     * @param assetType
+     * @param subType
+     * @param query
+     * @param attributes
+     * @return
+     */
+    public Iterable<ScatteredAsset> query(final String assetType, final String subType, final String query,
+            final String[] attributes) {
+        final Query q = new QueryBuilder(assetType, subType).condition(query).attributes(attributes).toQuery();
         return this.readAssets(q, mapper);
     }
 
