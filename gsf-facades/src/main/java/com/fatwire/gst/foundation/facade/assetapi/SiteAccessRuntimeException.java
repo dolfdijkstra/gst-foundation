67acc13026b3d222d8e1ca386a741e6ec94269a7
Dolf Dijkstra <Dolf.Dijkstra@xs4all.nl>
2011-04-13 19:08:17 +0000

    Added new constructor

--- gsf-facades/src/main/java/com/fatwire/gst/foundation/facade/assetapi/SiteAccessRuntimeException.java
+++ gsf-facades/src/main/java/com/fatwire/gst/foundation/facade/assetapi/SiteAccessRuntimeException.java
@@ -18,18 +18,27 @@ package com.fatwire.gst.foundation.facade.assetapi;
 
 import com.fatwire.assetapi.common.SiteAccessException;
 
-
-
 /**
  * 
  * RuntimeException adaptor for SiteAccessException.
+ * 
  * @author Dolf Dijkstra
  * @since Apr 6, 2011
  */
 public class SiteAccessRuntimeException extends RuntimeException {
 
-    public SiteAccessRuntimeException(SiteAccessException e) {
-        super(e);
+    /**
+     * @param cause the cause
+     */
+    public SiteAccessRuntimeException(SiteAccessException cause) {
+        super(cause);
+    }
+
+    /**
+     * @param msg the detail message for the exception
+     */
+    public SiteAccessRuntimeException(String msg) {
+        super(msg);
     }
 
     /**
