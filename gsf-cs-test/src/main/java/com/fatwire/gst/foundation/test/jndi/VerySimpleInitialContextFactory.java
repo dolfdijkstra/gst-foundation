adb87e9185f09f00c2b595f73411b8a68ac1fa22
Dolf Dijkstra <Dolf.Dijkstra@xs4all.nl>
2011-04-26 12:50:44 +0000

    Adding @SuppressWarnings("rawtypes") where indicated by compiler warnings

--- gsf-cs-test/src/main/java/com/fatwire/gst/foundation/test/jndi/VerySimpleInitialContextFactory.java
+++ gsf-cs-test/src/main/java/com/fatwire/gst/foundation/test/jndi/VerySimpleInitialContextFactory.java
@@ -31,181 +31,162 @@ import javax.naming.NamingException;
 /**
  * 
  * @author Dolf Dijkstra
- *
+ * 
  */
-public class VerySimpleInitialContextFactory implements
-		javax.naming.spi.InitialContextFactory {
-	private static Context initial;
+public class VerySimpleInitialContextFactory implements javax.naming.spi.InitialContextFactory {
+    private static Context initial;
 
-	public VerySimpleInitialContextFactory() {
-		super();
+    public VerySimpleInitialContextFactory() {
+        super();
 
-	}
+    }
 
-	@SuppressWarnings("unchecked")
-	public Context getInitialContext(final Hashtable<?, ?> environment)
-			throws NamingException {
-		//System.out.println("getInitialContext" + environment);
-		if (initial == null)
-			initial = new Context() {
-				Hashtable env = new Hashtable(environment);
-				Map<String, Object> store = new HashMap<String, Object>();
+    @SuppressWarnings("unchecked")
+    public Context getInitialContext(final Hashtable<?, ?> environment) throws NamingException {
+        // System.out.println("getInitialContext" + environment);
+        if (initial == null)
+            initial = new Context() {
+                @SuppressWarnings("rawtypes")
+                Hashtable env = new Hashtable(environment);
+                Map<String, Object> store = new HashMap<String, Object>();
 
-				public Object addToEnvironment(String propName, Object propVal)
-						throws NamingException {
-					return env.put(propName, propVal);
-				}
+                public Object addToEnvironment(String propName, Object propVal) throws NamingException {
+                    return env.put(propName, propVal);
+                }
 
-				public void bind(Name name, Object obj) throws NamingException {
-					store.put(name.toString(), obj);
+                public void bind(Name name, Object obj) throws NamingException {
+                    store.put(name.toString(), obj);
 
-				}
+                }
 
-				public void bind(String name, Object obj)
-						throws NamingException {
-					store.put(name.toString(), obj);
+                public void bind(String name, Object obj) throws NamingException {
+                    store.put(name.toString(), obj);
 
-				}
+                }
 
-				public void close() throws NamingException {
-					store.clear();
+                public void close() throws NamingException {
+                    store.clear();
 
-				}
+                }
 
-				public Name composeName(Name name, Name prefix)
-						throws NamingException {
+                public Name composeName(Name name, Name prefix) throws NamingException {
 
-					return null;
-				}
+                    return null;
+                }
 
-				public String composeName(String name, String prefix)
-						throws NamingException {
+                public String composeName(String name, String prefix) throws NamingException {
 
-					return null;
-				}
+                    return null;
+                }
 
-				public Context createSubcontext(Name name)
-						throws NamingException {
+                public Context createSubcontext(Name name) throws NamingException {
 
-					return null;
-				}
+                    return null;
+                }
 
-				public Context createSubcontext(String name)
-						throws NamingException {
+                public Context createSubcontext(String name) throws NamingException {
 
-					return null;
-				}
+                    return null;
+                }
 
-				public void destroySubcontext(Name name) throws NamingException {
+                public void destroySubcontext(Name name) throws NamingException {
 
-				}
+                }
 
-				public void destroySubcontext(String name)
-						throws NamingException {
+                public void destroySubcontext(String name) throws NamingException {
 
-				}
+                }
 
-				public Hashtable<?, ?> getEnvironment() throws NamingException {
-					return env;
-				}
+                public Hashtable<?, ?> getEnvironment() throws NamingException {
+                    return env;
+                }
 
-				public String getNameInNamespace() throws NamingException {
+                public String getNameInNamespace() throws NamingException {
 
-					return null;
-				}
+                    return null;
+                }
 
-				public NameParser getNameParser(Name name)
-						throws NamingException {
+                public NameParser getNameParser(Name name) throws NamingException {
 
-					return null;
-				}
+                    return null;
+                }
 
-				public NameParser getNameParser(String name)
-						throws NamingException {
+                public NameParser getNameParser(String name) throws NamingException {
 
-					return null;
-				}
+                    return null;
+                }
 
-				public NamingEnumeration<NameClassPair> list(Name name)
-						throws NamingException {
+                public NamingEnumeration<NameClassPair> list(Name name) throws NamingException {
 
-					return null;
-				}
+                    return null;
+                }
 
-				public NamingEnumeration<NameClassPair> list(String name)
-						throws NamingException {
+                public NamingEnumeration<NameClassPair> list(String name) throws NamingException {
 
-					return null;
-				}
+                    return null;
+                }
 
-				public NamingEnumeration<Binding> listBindings(Name name)
-						throws NamingException {
+                public NamingEnumeration<Binding> listBindings(Name name) throws NamingException {
 
-					return null;
-				}
+                    return null;
+                }
 
-				public NamingEnumeration<Binding> listBindings(String name)
-						throws NamingException {
+                public NamingEnumeration<Binding> listBindings(String name) throws NamingException {
 
-					return null;
-				}
+                    return null;
+                }
 
-				public Object lookup(Name name) throws NamingException {
-					return store.get(name.toString());
-				}
+                public Object lookup(Name name) throws NamingException {
+                    return store.get(name.toString());
+                }
 
-				public Object lookup(String name) throws NamingException {
-					return store.get(name);
-				}
+                public Object lookup(String name) throws NamingException {
+                    return store.get(name);
+                }
 
-				public Object lookupLink(Name name) throws NamingException {
+                public Object lookupLink(Name name) throws NamingException {
 
-					return null;
-				}
+                    return null;
+                }
 
-				public Object lookupLink(String name) throws NamingException {
+                public Object lookupLink(String name) throws NamingException {
 
-					return null;
-				}
+                    return null;
+                }
 
-				public void rebind(Name name, Object obj)
-						throws NamingException {
-					store.put(name.toString(), obj);
+                public void rebind(Name name, Object obj) throws NamingException {
+                    store.put(name.toString(), obj);
 
-				}
+                }
 
-				public void rebind(String name, Object obj)
-						throws NamingException {
-					store.put(name, obj);
+                public void rebind(String name, Object obj) throws NamingException {
+                    store.put(name, obj);
 
-				}
+                }
 
-				public Object removeFromEnvironment(String propName)
-						throws NamingException {
+                public Object removeFromEnvironment(String propName) throws NamingException {
 
-					return null;
-				}
+                    return null;
+                }
 
-				public void rename(Name oldName, Name newName)
-						throws NamingException {
+                public void rename(Name oldName, Name newName) throws NamingException {
 
-				}
+                }
 
-				public void rename(String oldName, String newName)
-						throws NamingException {
+                public void rename(String oldName, String newName) throws NamingException {
 
-				}
+                }
 
-				public void unbind(Name name) throws NamingException {
-					store.remove(name.toString());
+                public void unbind(Name name) throws NamingException {
+                    store.remove(name.toString());
 
-				}
+                }
 
-				public void unbind(String name) throws NamingException {
-					store.remove(name);
+                public void unbind(String name) throws NamingException {
+                    store.remove(name);
 
-				}
-			};
-		return initial;
-	}
+                }
+            };
+        return initial;
+    }
 }
