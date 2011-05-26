d64a3dde089d9f9ac01cc79f47d7552f7bd9c845
Dolf Dijkstra <Dolf.Dijkstra@xs4all.nl>
2011-05-26 07:39:58 +0000

    Removed unused import

--- gsf-facades/src/main/java/com/fatwire/gst/foundation/facade/assetapi/asset/ScatteredAsset.java
+++ gsf-facades/src/main/java/com/fatwire/gst/foundation/facade/assetapi/asset/ScatteredAsset.java
@@ -21,7 +21,6 @@ import java.util.AbstractMap;
 import java.util.Collection;
 import java.util.Date;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
