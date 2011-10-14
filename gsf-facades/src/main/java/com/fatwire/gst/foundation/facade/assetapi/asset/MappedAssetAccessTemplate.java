f3ae044299a74f57a42c071dcd21f2d7ff7dc1ff
Dolf Dijkstra <Dolf.Dijkstra@xs4all.nl>
2011-10-14 08:37:56 +0000

    Typo in javadoc

--- gsf-facades/src/main/java/com/fatwire/gst/foundation/facade/assetapi/asset/MappedAssetAccessTemplate.java
+++ gsf-facades/src/main/java/com/fatwire/gst/foundation/facade/assetapi/asset/MappedAssetAccessTemplate.java
@@ -101,7 +101,7 @@ public class MappedAssetAccessTemplate<T> extends AssetAccessTemplate {
     }
 
     /**
-     * Read the assets that are parents of the current asset givne the specified
+     * Read the assets that are parents of the current asset given the specified
      * association name or names.  Note this can result in an unknown dependency
      * @param id the child asset
      * @param associationName the association name or names to be used to filter parent query.
