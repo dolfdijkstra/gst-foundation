0f9296c5b82ec711308a40f2a63cfa5fe05d1ee4
Dolf Dijkstra <Dolf.Dijkstra@xs4all.nl>
2011-11-01 10:05:51 +0000

    Add utility class to work with loggers. Initial method is to help with loggers naming convention, based on package name of the class.

---
+++ gsf-facades/src/main/java/com/fatwire/gst/foundation/facade/logging/LogUtil.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2008 FatWire Corporation. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.fatwire.gst.foundation.facade.logging;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * Utility class for Loggers.
+ * 
+ * @author Dolf Dijkstra
+ * 
+ */
+public class LogUtil {
+
+    /**
+     * Returns the Log for the provided class based on the package name of the
+     * class.
+     * 
+     * @param clazz
+     * @return
+     */
+    public static Log getLog(Class<?> clazz) {
+        return LogFactory.getLog(clazz.getPackage().getName());
+    }
+
+}
