7399b6ae2ec3c1335cd0f28282daa42116cd7e4e
Dolf Dijkstra <Dolf.Dijkstra@xs4all.nl>
2011-11-02 13:25:36 +0000

    Adding listener that just prints to log it received an event.

---
+++ gsf-facades/src/main/java/com/fatwire/gst/foundation/facade/assetapi/listener/EventPrintAssetListener.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2008 FatWire Corporation. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.fatwire.gst.foundation.facade.assetapi.listener;
+
+import COM.FutureTense.Interfaces.ICS;
+
+import com.fatwire.assetapi.data.AssetId;
+import com.fatwire.gst.foundation.facade.install.AssetListenerInstall;
+import com.openmarket.basic.event.AbstractAssetEventListener;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class EventPrintAssetListener extends AbstractAssetEventListener {
+    private static final Log LOG = LogFactory.getLog(EventPrintAssetListener.class.getPackage().getName());
+
+    @Override
+    public void assetAdded(final AssetId id) {
+        LOG.info("Asset added " + id + " (" + Thread.currentThread().getName() + ")");
+    }
+
+    @Override
+    public void assetDeleted(final AssetId id) {
+        LOG.info("Asset deleted " + id + " (" + Thread.currentThread().getName() + ")");
+    }
+
+    @Override
+    public void assetUpdated(final AssetId id) {
+        LOG.info("Asset updated " + id + " (" + Thread.currentThread().getName() + ")");
+    }
+
+    public void install(final ICS ics) {
+        AssetListenerInstall.register(ics, EventPrintAssetListener.class.getName(), true);
+    }
+
+}
