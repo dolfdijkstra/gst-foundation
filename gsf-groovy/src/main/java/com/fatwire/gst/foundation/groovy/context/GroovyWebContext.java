0bcfde0a3540a871595c2d6756311c92cee557f0
Dolf Dijkstra <Dolf.Dijkstra@xs4all.nl>
2012-05-11 05:41:10 +0000

    Remove dead code

--- gsf-groovy/src/main/java/com/fatwire/gst/foundation/groovy/context/GroovyWebContext.java
+++ gsf-groovy/src/main/java/com/fatwire/gst/foundation/groovy/context/GroovyWebContext.java
@@ -67,7 +67,6 @@ public class GroovyWebContext extends DefaultWebAppContext {
     }
 
     protected ActionLocator getRootActionLocator(Injector injector) {
-        // Injector injector = getBean("injector", Injector.class);
         return new RenderPageActionLocator(injector);
     }
 }
