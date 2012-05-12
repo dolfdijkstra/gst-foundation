3eff3752e396392a83a97a4c9ad3939183c72d5c
Dolf Dijkstra <Dolf.Dijkstra@xs4all.nl>
2012-05-12 09:17:47 +0000

    typo

--- gsf-action/src/main/java/com/fatwire/gst/foundation/controller/annotation/IcsVariable.java
+++ gsf-action/src/main/java/com/fatwire/gst/foundation/controller/annotation/IcsVariable.java
@@ -23,7 +23,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Annatation to be used in a Controller to map requests to controller methods.
+ * Annotation to be used in a Controller to map requests to controller methods.
  * 
  * @author Dolf Dijkstra
  * @since Mar 21, 2011
