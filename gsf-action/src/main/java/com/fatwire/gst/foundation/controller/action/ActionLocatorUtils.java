ceeeaccc73bd360fcbd03566d75e2f51025b8d2c
Dolf Dijkstra <Dolf.Dijkstra@xs4all.nl>
2012-06-08 09:32:24 +0000

    Removing old code block

--- gsf-action/src/main/java/com/fatwire/gst/foundation/controller/action/ActionLocatorUtils.java
+++ gsf-action/src/main/java/com/fatwire/gst/foundation/controller/action/ActionLocatorUtils.java
@@ -20,17 +20,11 @@ import javax.servlet.ServletContext;
 
 import com.fatwire.gst.foundation.controller.support.WebContextUtil;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.WebApplicationContextUtils;
-
 /**
  * @author Dolf Dijkstra
  * @since Apr 11, 2011
  */
 public final class ActionLocatorUtils {
-    private static final Log LOG = LogFactory.getLog(ActionLocatorUtils.class.getPackage().getName());
     public static final String ACTION_LOCATOR_BEAN = "gsfActionLocator";
 
     /**
@@ -40,8 +34,8 @@ public final class ActionLocatorUtils {
     }
 
     /**
-     * Returns the ActionLocator as configured on the
-     * WebAppContext bean by the name of gsfActionLocator.
+     * Returns the ActionLocator as configured on the WebAppContext bean by the
+     * name of gsfActionLocator.
      * 
      * @param servletContext the servlet context.
      * @return the ActionLocator that is configured via the servletContext.
@@ -51,8 +45,8 @@ public final class ActionLocatorUtils {
     }
 
     /**
-     * Returns the ActionLocator as configured on the
-     * WebAppContext bean by the name 'beanName'.
+     * Returns the ActionLocator as configured on the WebAppContext bean by the
+     * name 'beanName'.
      * 
      * @param servletContext the servlet context.
      * @return the ActionLocator that is configured via the servletContext.
@@ -60,32 +54,6 @@ public final class ActionLocatorUtils {
 
     public static ActionLocator getActionLocator(final ServletContext servletContext, String beanName) {
         return WebContextUtil.getWebAppContext(servletContext).getBean(beanName, ActionLocator.class);
-    }
-    /**
-     * Returns the ActionLocator as configured by spring framework on the
-     * WebApplicationContext bean by the name 'beanName'.
-     * 
-     * @param servletContext the servlet context.
-     * @return the ActionLocator that is configured via the servletContext.
-     */
-    public static ActionLocator getActionLocatorX(final ServletContext servletContext, String beanName) {
-
-        // get the spring web application context
-        final WebApplicationContext wac = WebApplicationContextUtils.getRequiredWebApplicationContext(servletContext);
-
-        // get the bean.
-
-        final ActionLocator locator;
-        if (wac.containsBean(beanName)) {
-            locator = (ActionLocator) wac.getBean(beanName);
-            if (LOG.isTraceEnabled()) {
-                LOG.trace("Using actionLocatorBean as configured: " + locator.getClass().getName());
-            }
-        } else {
-            LOG.warn("Cannot find the '" + beanName + "' bean in the Spring WebApplicationContext: " + wac.toString());
-            locator = null;
-        }
-        return locator;
     }
 
 }
