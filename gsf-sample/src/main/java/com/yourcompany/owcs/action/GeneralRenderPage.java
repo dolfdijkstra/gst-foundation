035704d5d4b828ed7433c15ded87d95e8152d93f
Tony Field <tony.field@metastratus.com>
2012-08-24 13:39:27 -0500

    Cleaned up package names and added a placeholder WebAppContext.

--- gsf-sample/src/main/java/com/yourcompany/owcs/action/GeneralRenderPage.java
+++ gsf-sample/src/main/java/com/yourcompany/owcs/action/GeneralRenderPage.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.yourcompany.owcs.config;
+package com.yourcompany.owcs.action;
 
 import COM.FutureTense.Interfaces.ICS;
 
