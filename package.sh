6cab59c74645ecf40cf3384876ddecf73a6385fb
Dolf Dijkstra <dolf.dijkstra@xs4all.nl>
2012-08-27 15:20:24 +0200

    Improvements, building of build-tools, tar has a versioned subdir now

--- package.sh
+++ package.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
-VERSION=11.0-SNAPSHOT
+set -o nounset
+set -o errexit
+
+VERSION=11.6.0-SNAPSHOT
+if [ ! -d $HOME/.m2/repository/com/fatwire/gst/gst-foundation-all ] ;
+then 
+   echo The GSF artifacts seem not the be present in your maven  maven repository, this is expected if you are building for the first time on this computer
+   echo Starting initial build
+   echo
+   (cd gsf-build-tools && mvn -q -Dmaven.test.skip=true install && cd .. && mvn install)
+   echo  finished initial build
+fi
 echo downloading all artifacts
 mvn -q dependency:go-offline
 echo building jars and site
-mvn -o -q -pl gsf-build-tools install 
 mvn -o -q clean install site
-if [ -d /tmp/gsf-site ] ; then rm -Rf /tmp/gsf-site ;fi
-mvn -o site:stage -DstagingDirectory=/tmp/gsf-site
+tmpLocation=/tmp/gsf-$VERSION
+if [ -d "$tmpLocation" ] ; then rm -Rf "$tmpLocation" ;fi
+mvn -o site:stage -DstagingDirectory="$tmpLocation/site"
 
 archive=`pwd`/target/gst-foundation-$VERSION.tgz 
-
-(cd gst-foundation-all/target  && tar -czf $archive gst* -C /tmp/ gsf-site )
+ 
+cp -R gst-foundation-all/target/gst* "$tmpLocation"
+cp LICENSE "$tmpLocation"
+(cd /tmp && tar -czf $archive gsf-* ) 
 
 echo done packaging gst-foundation
 # to get a graphical dependency tree: http://www.summa-tech.com/blog/2011/04/12/a-visual-maven-dependency-tree-view/
@@ -37,3 +50,6 @@ echo done packaging gst-foundation
 # The LicenseHeader.txt does not contain the copyright statement, as this can be different for each file. This file is used
 # to trick the license plugin and not barf over different copyright statements. 
 # you can issue mvn license:format -Dcompany="My Corporation" to add header files with a copyright statement to all files that do not have a license header.
+
+# quick check if a copyright statement is present
+# find . -name '*.java'  ! -exec egrep -q 'Copyright .* All Rights Reserved' {} \; -ls
