a0ddfbb65c7e7703365b9f3f865245c9d98e65ab
Dolf Dijkstra <dolf.dijkstra@xs4all.nl>
2012-10-24 11:26:13 +0200

    copyright and javadoc

--- gsf-facades/src/main/java/com/fatwire/gst/foundation/facade/logging/Log.java
+++ gsf-facades/src/main/java/com/fatwire/gst/foundation/facade/logging/Log.java
@@ -1,5 +1,27 @@
+/*
+ * Copyright 2012 oracle Corporation. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.fatwire.gst.foundation.facade.logging;
 
+/**
+ * Extension of commons logging to allow for log messages with arbitrary number of arguments, elleviating the developer from string concatenation and level checking to prevent expensive string contatenation.    
+ * 
+ * @author Dolf Dijkstra
+ *
+ */
 public interface Log extends org.apache.commons.logging.Log {
 
     public void trace(String message, Throwable t, Object... args);
