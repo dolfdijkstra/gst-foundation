ded4bb73525c0a57df9d00127c3b4983060e0333
Dolf Dijkstra <dolf.dijkstra@xs4all.nl>
2013-12-02 10:41:47 +0100

    Removing unused logger

--- gsf-taglib/src/main/java/com/fatwire/gst/foundation/taglib/GsfRootTag.java
+++ gsf-taglib/src/main/java/com/fatwire/gst/foundation/taglib/GsfRootTag.java
@@ -26,68 +26,69 @@ import COM.FutureTense.Interfaces.ICS;
 import com.fatwire.gst.foundation.facade.assetapi.asset.ScatteredAssetAccessTemplate;
 import com.fatwire.gst.foundation.facade.runtag.render.LogDep;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
 public class GsfRootTag extends BodyTagSupport {
-    public static final String ICS_VARIABLE_NAME = "ics";
-    public static final String VARIABLE_SCOPE_NAME = "cs";
-    public static final String ASSET_DAO = "assetDao";
-    /**
+	public static final String ICS_VARIABLE_NAME = "ics";
+	public static final String VARIABLE_SCOPE_NAME = "cs";
+	public static final String ASSET_DAO = "assetDao";
+	/**
 	 * 
 	 */
-    private static final long serialVersionUID = -5369419132504852400L;
+	private static final long serialVersionUID = -5369419132504852400L;
 
-    private static final Log LOG = LogFactory.getLog(GsfRootTag.class.getPackage().getName());
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see COM.FutureTense.JspTags.Root#doStartTag()
+	 */
+	@Override
+	public int doStartTag() throws JspException {
+		super.doStartTag();
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see COM.FutureTense.JspTags.Root#doStartTag()
-     */
-    @Override
-    public int doStartTag() throws JspException {
-        super.doStartTag();
+		final ICS ics = getICS();
 
-        final ICS ics = getICS();
+		if (ics != null) {
+			pageContext.setAttribute(VARIABLE_SCOPE_NAME, new ICSAsMap(ics),
+					PageContext.PAGE_SCOPE);
+			final ScatteredAssetAccessTemplate assetTemplate = new ScatteredAssetAccessTemplate(
+					ics);
+			pageContext.setAttribute(ASSET_DAO, assetTemplate,
+					PageContext.PAGE_SCOPE);
+			if (ics.GetVar("tid") != null) {
+				LogDep.logDep(ics, "Template", ics.GetVar("tid"));
+			}
+			if (ics.GetVar("seid") != null) {
+				LogDep.logDep(ics, "SiteEntry", ics.GetVar("seid"));
+			}
+			if (ics.GetVar("eid") != null) {
+				LogDep.logDep(ics, "CSElement", ics.GetVar("eid"));
+			}
+		} else {
+			throw new JspException(
+					"ics is not found on the page context. This tags needs to be nested in the <cs:ftcs> tag.");
+		}
+		return EVAL_BODY_INCLUDE;
+	}
 
-        if (ics != null) {
-            pageContext.setAttribute(VARIABLE_SCOPE_NAME, new ICSAsMap(ics), PageContext.PAGE_SCOPE);
-            final ScatteredAssetAccessTemplate assetTemplate = new ScatteredAssetAccessTemplate(ics);
-            pageContext.setAttribute(ASSET_DAO, assetTemplate, PageContext.PAGE_SCOPE);
-            if (ics.GetVar("tid") != null) {
-                LogDep.logDep(ics, "Template", ics.GetVar("tid"));
-            }
-            if (ics.GetVar("seid") != null) {
-                LogDep.logDep(ics, "SiteEntry", ics.GetVar("seid"));
-            }
-            if (ics.GetVar("eid") != null) {
-                LogDep.logDep(ics, "CSElement", ics.GetVar("eid"));
-            }
-        } else {
-            throw new JspException("ics is not found on the page context. This tags needs to be nested in the <cs:ftcs> tag.");
-        }
-        return EVAL_BODY_INCLUDE;
-    }
+	protected ICS getICS() {
+		final Object o = pageContext.getAttribute(ICS_VARIABLE_NAME,
+				PageContext.PAGE_SCOPE);
+		if (o instanceof ICS) {
+			return (ICS) o;
+		}
+		return null;
+	}
 
-    protected ICS getICS() {
-        final Object o = pageContext.getAttribute(ICS_VARIABLE_NAME, PageContext.PAGE_SCOPE);
-        if (o instanceof ICS) {
-            return (ICS) o;
-        }
-        return null;
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see javax.servlet.jsp.tagext.BodyTagSupport#doEndTag()
-     */
-    @Override
-    public int doEndTag() throws JspException {
-        pageContext.removeAttribute(VARIABLE_SCOPE_NAME, PageContext.PAGE_SCOPE);
-        pageContext.removeAttribute(ASSET_DAO, PageContext.PAGE_SCOPE);
-        return Tag.EVAL_PAGE;
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.servlet.jsp.tagext.BodyTagSupport#doEndTag()
+	 */
+	@Override
+	public int doEndTag() throws JspException {
+		pageContext
+				.removeAttribute(VARIABLE_SCOPE_NAME, PageContext.PAGE_SCOPE);
+		pageContext.removeAttribute(ASSET_DAO, PageContext.PAGE_SCOPE);
+		return Tag.EVAL_PAGE;
+	}
 
 }
