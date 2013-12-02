75ddcaff559c23a9982fbb474429f2f2bbbd10ab
Dolf Dijkstra <dolf.dijkstra@xs4all.nl>
2013-12-02 10:54:21 +0100

    Dropping lookup of ICS as this is problematic wrt the ICS life cycle.

--- gsf-wra/src/main/java/com/fatwire/gst/foundation/wra/WraCoreFieldApiBypassDao.java
+++ gsf-wra/src/main/java/com/fatwire/gst/foundation/wra/WraCoreFieldApiBypassDao.java
@@ -47,203 +47,216 @@ import org.apache.commons.logging.LogFactory;
  */
 public class WraCoreFieldApiBypassDao extends AssetApiWraCoreFieldDao {
 
-    /**
-     * @deprecated makes unsafe calls and refrences to unsupported ICS objects
-     * @param ics
-     * @return
-     */
-    @Deprecated
-    public static WraCoreFieldApiBypassDao getBackdoorInstance(ICS ics) {
-        if (ics == null) {
-            ics = ICSFactory.getOrCreateICS();
-        }
+	/**
+	 * @deprecated makes unsafe calls and references to unsupported ICS objects
+	 * @param ics
+	 * @return
+	 */
+	@Deprecated
+	public static WraCoreFieldApiBypassDao getBackdoorInstance(ICS ics) {
 
-        Object o = ics.GetObj(WraCoreFieldApiBypassDao.class.getName());
-        if (o == null) {
-            o = new WraCoreFieldApiBypassDao(ics);
-            ics.SetObj(WraCoreFieldApiBypassDao.class.getName(), o);
-        }
-        return (WraCoreFieldApiBypassDao) o;
-    }
+		Object o = ics.GetObj(WraCoreFieldApiBypassDao.class.getName());
+		if (o == null) {
+			o = new WraCoreFieldApiBypassDao(ics);
+			ics.SetObj(WraCoreFieldApiBypassDao.class.getName(), o);
+		}
+		return (WraCoreFieldApiBypassDao) o;
+	}
 
-    private final ICS ics;
-    private final DirectSqlAccessTools directSqlAccessTools;
+	private final ICS ics;
+	private final DirectSqlAccessTools directSqlAccessTools;
 
-    private WraCoreFieldApiBypassDao(final ICS ics) {
-        super(ics);
-        this.ics = ics;
-        directSqlAccessTools = new DirectSqlAccessTools(ics);
-    }
+	private WraCoreFieldApiBypassDao(final ICS ics) {
+		super(ics);
+		this.ics = ics;
+		directSqlAccessTools = new DirectSqlAccessTools(ics);
+	}
 
-    private static final Log LOG = LogFactory.getLog(WraCoreFieldApiBypassDao.class);
+	private static final Log LOG = LogFactory
+			.getLog(WraCoreFieldApiBypassDao.class);
 
-    /**
-     * Method to test whether or not an asset is web-referenceable. todo: low
-     * priority: optimize as this will be called at runtime (assest api incache
-     * will mitigate the performance issue)
-     * 
-     * @param id asset ID to check
-     * @return true if the asset is a valid web-referenceable asset, false if it
-     *         is not
-     */
-    @Override
-    public boolean isWebReferenceable(final AssetId id) {
+	/**
+	 * Method to test whether or not an asset is web-referenceable. todo: low
+	 * priority: optimize as this will be called at runtime (assest api incache
+	 * will mitigate the performance issue)
+	 * 
+	 * @param id
+	 *            asset ID to check
+	 * @return true if the asset is a valid web-referenceable asset, false if it
+	 *         is not
+	 */
+	@Override
+	public boolean isWebReferenceable(final AssetId id) {
 
-        if (directSqlAccessTools.isFlex(id)) {
-            if (!isWraEnabledFlexAssetType(id)) {
-                return false;
-            }
-        } else {
-            if (!isWraEnabledBasicAssetType(id)) {
-                return false;
-            }
+		if (directSqlAccessTools.isFlex(id)) {
+			if (!isWraEnabledFlexAssetType(id)) {
+				return false;
+			}
+		} else {
+			if (!isWraEnabledBasicAssetType(id)) {
+				return false;
+			}
 
-        }
-        // type is wra, now lookup the asset data
-        try {
-            final WebReferenceableAsset wra = getWra(id);
-            final boolean b = StringUtils.isNotBlank(wra.getPath());
-            if (LOG.isTraceEnabled()) {
-                LOG.trace("Asset " + id + (b ? " is " : " is not ")
-                        + "web-referenceable, as determinted by the presence of a path attribute.");
-            }
-            return b;
-        } catch (final RuntimeException e) {
-            if (LOG.isTraceEnabled()) {
-                LOG.trace("Asset " + id + " is not web-referenceable: " + e, e);
-            }
-            return false;
-        }
-    }
+		}
+		// type is wra, now lookup the asset data
+		try {
+			final WebReferenceableAsset wra = getWra(id);
+			final boolean b = StringUtils.isNotBlank(wra.getPath());
+			if (LOG.isTraceEnabled()) {
+				LOG.trace("Asset "
+						+ id
+						+ (b ? " is " : " is not ")
+						+ "web-referenceable, as determinted by the presence of a path attribute.");
+			}
+			return b;
+		} catch (final RuntimeException e) {
+			if (LOG.isTraceEnabled()) {
+				LOG.trace("Asset " + id + " is not web-referenceable: " + e, e);
+			}
+			return false;
+		}
+	}
 
-    private boolean isWraEnabledFlexAssetType(final AssetId id) {
-        // TODO:medium implements proper check
-        return true;
-    }
+	private boolean isWraEnabledFlexAssetType(final AssetId id) {
+		// TODO:medium implements proper check
+		return true;
+	}
 
-    /**
-     * Return a web referenceable asset bean given an input id. Required fields
-     * must be set or an exception is thrown.
-     * 
-     * @param id asset id
-     * @return WebReferenceableAsset, never null
-     * @see #isWebReferenceable(AssetId)
-     */
-    @Override
-    public WebReferenceableAsset getWra(final AssetId id) {
-        if (directSqlAccessTools.isFlex(id)) {
-            // todo: medium: optimize as this is very inefficient for flex
-            // assets
-            final PreparedStmt basicFields = new PreparedStmt(
-                    "SELECT id,name,description,subtype,status,path,template,startdate,enddate" + " FROM "
-                            + id.getType() + " WHERE id = ?", Collections.singletonList(id.getType()));
-            basicFields.setElement(0, id.getType(), "id");
+	/**
+	 * Return a web referenceable asset bean given an input id. Required fields
+	 * must be set or an exception is thrown.
+	 * 
+	 * @param id
+	 *            asset id
+	 * @return WebReferenceableAsset, never null
+	 * @see #isWebReferenceable(AssetId)
+	 */
+	@Override
+	public WebReferenceableAsset getWra(final AssetId id) {
+		if (directSqlAccessTools.isFlex(id)) {
+			// todo: medium: optimize as this is very inefficient for flex
+			// assets
+			final PreparedStmt basicFields = new PreparedStmt(
+					"SELECT id,name,description,subtype,status,path,template,startdate,enddate"
+							+ " FROM " + id.getType() + " WHERE id = ?",
+					Collections.singletonList(id.getType()));
+			basicFields.setElement(0, id.getType(), "id");
 
-            final StatementParam param = basicFields.newParam();
-            param.setLong(0, id.getId());
-            final Row row = SqlHelper.selectSingle(ics, basicFields, param);
+			final StatementParam param = basicFields.newParam();
+			param.setLong(0, id.getId());
+			final Row row = SqlHelper.selectSingle(ics, basicFields, param);
 
-            final WraBeanImpl wra = new WraBeanImpl();
-            wra.setId(id);
-            wra.setName(row.getString("name"));
-            wra.setDescription(row.getString("description"));
-            wra.setSubtype(row.getString("subtype"));
-            wra.setPath(row.getString("path"));
-            wra.setTemplate(row.getString("template"));
-            if (StringUtils.isNotBlank(row.getString("startdate"))) {
-                wra.setStartDate(row.getDate("startdate"));
-            }
-            if (StringUtils.isNotBlank(row.getString("enddate"))) {
-                wra.setEndDate(row.getDate("enddate"));
-            }
+			final WraBeanImpl wra = new WraBeanImpl();
+			wra.setId(id);
+			wra.setName(row.getString("name"));
+			wra.setDescription(row.getString("description"));
+			wra.setSubtype(row.getString("subtype"));
+			wra.setPath(row.getString("path"));
+			wra.setTemplate(row.getString("template"));
+			if (StringUtils.isNotBlank(row.getString("startdate"))) {
+				wra.setStartDate(row.getDate("startdate"));
+			}
+			if (StringUtils.isNotBlank(row.getString("enddate"))) {
+				wra.setEndDate(row.getDate("enddate"));
+			}
 
-            wra.setMetaTitle(directSqlAccessTools.getFlexAttributeValue(id, "metatitle"));
-            wra.setMetaDescription(directSqlAccessTools.getFlexAttributeValue(id, "metadescription"));
-            wra.setMetaKeyword(directSqlAccessTools.getFlexAttributeValue(id, "metakeywords"));
-            wra.setH1Title(directSqlAccessTools.getFlexAttributeValue(id, "h1title"));
-            wra.setLinkText(directSqlAccessTools.getFlexAttributeValue(id, "linktext"));
+			wra.setMetaTitle(directSqlAccessTools.getFlexAttributeValue(id,
+					"metatitle"));
+			wra.setMetaDescription(directSqlAccessTools.getFlexAttributeValue(
+					id, "metadescription"));
+			wra.setMetaKeyword(directSqlAccessTools.getFlexAttributeValue(id,
+					"metakeywords"));
+			wra.setH1Title(directSqlAccessTools.getFlexAttributeValue(id,
+					"h1title"));
+			wra.setLinkText(directSqlAccessTools.getFlexAttributeValue(id,
+					"linktext"));
 
-            return wra;
-        } else {
+			return wra;
+		} else {
 
-            final PreparedStmt basicFields = new PreparedStmt(
-                    "SELECT id,name,description,subtype,status,path,template,startdate,enddate,"
-                            + "metatitle,metadescription,metakeyword,h1title,linktext FROM " + id.getType()
-                            + " WHERE id = ?", Collections.singletonList(id.getType()));
-            basicFields.setElement(0, id.getType(), "id");
+			final PreparedStmt basicFields = new PreparedStmt(
+					"SELECT id,name,description,subtype,status,path,template,startdate,enddate,"
+							+ "metatitle,metadescription,metakeyword,h1title,linktext FROM "
+							+ id.getType() + " WHERE id = ?",
+					Collections.singletonList(id.getType()));
+			basicFields.setElement(0, id.getType(), "id");
 
-            final StatementParam param = basicFields.newParam();
-            param.setLong(0, id.getId());
-            final Row row = SqlHelper.selectSingle(ics, basicFields, param);
+			final StatementParam param = basicFields.newParam();
+			param.setLong(0, id.getId());
+			final Row row = SqlHelper.selectSingle(ics, basicFields, param);
 
-            final WraBeanImpl wra = new WraBeanImpl();
-            wra.setId(id);
-            wra.setName(row.getString("name"));
-            wra.setDescription(row.getString("description"));
-            wra.setSubtype(row.getString("subtype"));
-            wra.setMetaTitle(row.getString("metatitle"));
-            wra.setMetaDescription(row.getString("metadescription"));
-            wra.setMetaKeyword(row.getString("metakeywords"));
-            wra.setH1Title(row.getString("h1title"));
-            wra.setLinkText(row.getString("linktext"));
-            wra.setPath(row.getString("path"));
-            wra.setTemplate(row.getString("template"));
-            if (StringUtils.isNotBlank(row.getString("startdate"))) {
-                wra.setStartDate(row.getDate("startdate"));
-            }
-            if (StringUtils.isNotBlank(row.getString("enddate"))) {
-                wra.setEndDate(row.getDate("enddate"));
-            }
-            return wra;
-        }
-    }
+			final WraBeanImpl wra = new WraBeanImpl();
+			wra.setId(id);
+			wra.setName(row.getString("name"));
+			wra.setDescription(row.getString("description"));
+			wra.setSubtype(row.getString("subtype"));
+			wra.setMetaTitle(row.getString("metatitle"));
+			wra.setMetaDescription(row.getString("metadescription"));
+			wra.setMetaKeyword(row.getString("metakeywords"));
+			wra.setH1Title(row.getString("h1title"));
+			wra.setLinkText(row.getString("linktext"));
+			wra.setPath(row.getString("path"));
+			wra.setTemplate(row.getString("template"));
+			if (StringUtils.isNotBlank(row.getString("startdate"))) {
+				wra.setStartDate(row.getDate("startdate"));
+			}
+			if (StringUtils.isNotBlank(row.getString("enddate"))) {
+				wra.setEndDate(row.getDate("enddate"));
+			}
+			return wra;
+		}
+	}
 
-    /**
-     * Checks if the table definition for a basic asset has all the wra fields.
-     * 
-     * @param id
-     */
-    private boolean isWraEnabledBasicAssetType(final AssetId id) {
-        boolean wraTable;
-        final String listname = IListUtils.generateRandomListName();
-        final IList list = ics.CatalogDef(id.getType(), listname, new StringBuffer());
-        ics.RegisterList(listname, null);
-        final List<String> attr = Arrays.asList(WRA_ATTRIBUTE_NAMES);
-        int count = 0;
-        for (final Row row : new IListIterable(list)) {
-            /*
-             * "COLNAME" "COLTYPE" "COLSIZE" "KEY"
-             */
-            if (attr.contains(row.getString("COLNAME").toLowerCase())) {
-                count++;
-            }
-        }
-        wraTable = count == attr.size();// all wra attributes are found in the
-        // table def.
-        if (LOG.isTraceEnabled()) {
-            LOG.trace("Asset " + id + (wraTable ? " is " : " is not ")
-                    + "web-referenceable, as determinted by the table definition.");
-        }
+	/**
+	 * Checks if the table definition for a basic asset has all the wra fields.
+	 * 
+	 * @param id
+	 */
+	private boolean isWraEnabledBasicAssetType(final AssetId id) {
+		boolean wraTable;
+		final String listname = IListUtils.generateRandomListName();
+		final IList list = ics.CatalogDef(id.getType(), listname,
+				new StringBuffer());
+		ics.RegisterList(listname, null);
+		final List<String> attr = Arrays.asList(WRA_ATTRIBUTE_NAMES);
+		int count = 0;
+		for (final Row row : new IListIterable(list)) {
+			/*
+			 * "COLNAME" "COLTYPE" "COLSIZE" "KEY"
+			 */
+			if (attr.contains(row.getString("COLNAME").toLowerCase())) {
+				count++;
+			}
+		}
+		wraTable = count == attr.size();// all wra attributes are found in the
+		// table def.
+		if (LOG.isTraceEnabled()) {
+			LOG.trace("Asset "
+					+ id
+					+ (wraTable ? " is " : " is not ")
+					+ "web-referenceable, as determinted by the table definition.");
+		}
 
-        return wraTable;
-    }
+		return wraTable;
+	}
 
-    @Override
-    public boolean isVanityAsset(AssetId id) {
-        try {
-            final PreparedStmt basicFields = new PreparedStmt("SELECT path FROM " + id.getType() + " WHERE id = ?",
-                    Collections.singletonList(id.getType()));
-            basicFields.setElement(0, id.getType(), "id");
+	@Override
+	public boolean isVanityAsset(AssetId id) {
+		try {
+			final PreparedStmt basicFields = new PreparedStmt(
+					"SELECT path FROM " + id.getType() + " WHERE id = ?",
+					Collections.singletonList(id.getType()));
+			basicFields.setElement(0, id.getType(), "id");
 
-            final StatementParam param = basicFields.newParam();
-            param.setLong(0, id.getId());
-            final Row row = SqlHelper.selectSingle(ics, basicFields, param);
-            if (row == null)
-                return false;
-            return StringUtils.isNotBlank(row.getString("path"));
-        } catch (RuntimeException e) {
-            return false;
-        }
-    }
+			final StatementParam param = basicFields.newParam();
+			param.setLong(0, id.getId());
+			final Row row = SqlHelper.selectSingle(ics, basicFields, param);
+			if (row == null)
+				return false;
+			return StringUtils.isNotBlank(row.getString("path"));
+		} catch (RuntimeException e) {
+			return false;
+		}
+	}
 
 }
