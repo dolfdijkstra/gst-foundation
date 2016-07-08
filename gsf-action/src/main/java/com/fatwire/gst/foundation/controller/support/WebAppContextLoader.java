46dc17e0af1f2d27a3955e88a1b163d0629d2ef8
Tony Field <tony@function1.com>
2016-07-08 12:50:40 -0400

    Configure AppContextLoader by default using listener annotation. Because it will ALWAYS be created, no need to lazily create one in the util class.

--- gsf-action/src/main/java/com/fatwire/gst/foundation/controller/support/WebAppContextLoader.java
+++ gsf-action/src/main/java/com/fatwire/gst/foundation/controller/support/WebAppContextLoader.java
@@ -29,6 +29,7 @@ import java.util.List;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
 
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
@@ -42,75 +43,75 @@ import com.fatwire.gst.foundation.controller.action.support.DefaultWebAppContext
  * application.
  * 
  * @author Dolf Dijkstra
- * 
+ * @author Tony Field
  */
+@WebListener
 public class WebAppContextLoader implements ServletContextListener {
     public static final String CONTEXTS = "gsf-contexts";
 
     protected static final Logger LOG = LoggerFactory.getLogger("tools.gsf.controller.support.WebAppContextLoader");
-    boolean booted = false;
     private static final Class<?>[] ARGS = new Class[] { ServletContext.class, AppContext.class };
 
-    @Override
     public void contextInitialized(final ServletContextEvent sce) {
         final ServletContext context = sce.getServletContext();
-        if (context.getMajorVersion() == 2 && context.getMinorVersion() < 4) {
-            throw new IllegalStateException(
-                    "Servlet Container is configured for version 2.3 or less. This ServletContextListener does not support 2.3 and earlier as the load order of Listeners is not guaranteed.");
+        if (context.getMajorVersion() == 3 && context.getMinorVersion() < 0) {
+            throw new IllegalStateException("Servlet Container is configured for version less than 3.0. This ServletContextListener does not support 2.x and earlier as the load order of Listeners is not guaranteed.");
         }
-
         configureWebAppContext(context);
-
     }
 
-    public AppContext configureWebAppContext(final ServletContext context) {
-        AppContext parent = null;
+    private AppContext configureWebAppContext(final ServletContext context) {
+
+        AppContext parent;
 
         final ClassLoader cl = Thread.currentThread().getContextClassLoader();
 
+        // start with init param
         parent = configureFromInitParam(context, cl);
+        if (parent != null) {
+            LOG.info("AppContext initialized from init param. Class: {}", parent.getClass().getName());
+        }
+
+        // try locator
         if (parent == null) {
-            try {
-                parent = configureFromServiceLocator(context, cl);
-            } catch (IOException e) {
-                LOG.debug("Exception when loadding the service descriptor for the AppContext from the classpath.", e);
+            parent = configureFromServiceLocator(context, cl);
+            if (parent != null) {
+                LOG.info("AppContext initialized from service locator. Class: {}", parent.getClass().getName());
             }
         }
+
+        // use default
         if (parent == null) {
             parent = new DefaultWebAppContext(context);
             parent.init();
+            LOG.info("AppContext initialized using default class: {}"+parent.getClass().getName());
         }
 
-        if (parent != null) {
-            context.setAttribute(WebAppContext.WEB_CONTEXT_NAME, parent);
-        }
-        booted = true;
+        // register it
+        context.setAttribute(WebAppContext.WEB_CONTEXT_NAME, parent);
+
         return parent;
-
     }
 
     private static final String PREFIX = "META-INF/";
 
-    private AppContext configureFromServiceLocator(ServletContext context, ClassLoader cl) throws IOException {
+    private AppContext configureFromServiceLocator(ServletContext context, ClassLoader cl) {
         String fullName = PREFIX + CONTEXTS;
 
-        int c = 0;
-        List<String> init = new LinkedList<String>();
-        Enumeration<URL> configs = cl.getResources(fullName);
-        while (configs.hasMoreElements()) {
+        List<String> init = new LinkedList<>();
+        Enumeration<URL> configs;
+        try {
+            configs = cl.getResources(fullName);
+        } catch (IOException e) {
+            LOG.warn("Exception when loading the service descriptor for the AppContext from the classpath.", e);
+            return null;
+        }
 
+        if (configs.hasMoreElements()) {
             URL u = configs.nextElement();
-            if (c++ > 0)
-                throw new IllegalStateException("Found second service locator in classpath at " + u
-                        + ". Please make sure that only one " + fullName
-                        + " file is found on the classpath or configure the AppContext through web.xml");
-            InputStream in = null;
-            BufferedReader r = null;
-
-            try {
-                in = u.openStream();
-                r = new BufferedReader(new InputStreamReader(in, "utf-8"));
-                String s = null;
+            try (InputStream in = u.openStream();
+                 BufferedReader r = new BufferedReader(new InputStreamReader(in, "utf-8"))) {
+                String s;
                 while ((s = r.readLine()) != null) {
                     if (StringUtils.isNotBlank(s) && !StringUtils.startsWith(s, "#")) {
                         init.add(s);
@@ -118,19 +119,9 @@ public class WebAppContextLoader implements ServletContextListener {
                 }
             } catch (IOException e) {
                 throw new RuntimeException("Error reading configuration file", e);
-            } finally {
-                try {
-                    if (r != null)
-                        r.close();
-                    if (in != null)
-                        in.close();
-                } catch (IOException e) {
-                    throw new RuntimeException("Error closing configuration file", e);
-                }
             }
             return this.createFromString(context, cl, init.toArray(new String[init.size()]));
         }
-
         return null;
     }
 
@@ -155,39 +146,24 @@ public class WebAppContextLoader implements ServletContextListener {
         AppContext parent = null;
 
         if (c != null) {
-
             for (int i = c.length - 1; i >= 0; i--) {
-
                 try {
                     final AppContext n = createAppContext(cl, c[i], context, parent);
                     if (n != null) {
                         parent = n;
                     }
-                } catch (final IllegalArgumentException e) {
+                } catch (IllegalArgumentException
+                        | InstantiationException
+                        | IllegalAccessException
+                        | InvocationTargetException
+                        | SecurityException
+                        | NoSuchMethodException
+                        | ClassNotFoundException e) {
                     LOG.warn("Couldn't create the app context", e);
-                } catch (final InstantiationException e) {
-                	LOG.warn("Couldn't create the app context", e);
-                } catch (final IllegalAccessException e) {
-                	LOG.warn("Couldn't create the app context", e);
-                } catch (final InvocationTargetException e) {
-                	LOG.warn("Couldn't create the app context", e);
-                } catch (final SecurityException e) {
-                	LOG.warn("Couldn't create the app context", e);
-                } catch (final NoSuchMethodException e) {
-                	LOG.warn("Couldn't create the app context", e);
-                } catch (final ClassNotFoundException e) {
-                	LOG.warn("Couldn't create the app context", e);
                 }
-
             }
-
         }
         return parent;
-    }
-
-    @Override
-    public void contextDestroyed(final ServletContextEvent sce) {
-        sce.getServletContext().removeAttribute(WebAppContext.WEB_CONTEXT_NAME);
     }
 
     /**
@@ -212,12 +188,13 @@ public class WebAppContextLoader implements ServletContextListener {
         final Constructor<AppContext> ctr = cls.getConstructor(ARGS);
         AppContext n;
         n = ctr.newInstance(context, parent);
-        if (n != null) {
-            LOG.info("Creating AppContext from class " + c);
-            n.init();
-        }
+        n.init();
         return n;
+    }
 
+    public void contextDestroyed(final ServletContextEvent sce) {
+        sce.getServletContext().removeAttribute(WebAppContext.WEB_CONTEXT_NAME);
+        LOG.info("AppContext un-registered from servlet context.");
     }
 
 }
